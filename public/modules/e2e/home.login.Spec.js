ebaf4c8e3c4f3b6c0002a1d048737958e6e85e83
andela-amaborukoje <adebayo.maborukoje@andela.co>
2015-02-10 15:47:54 +0100

    commented unwanted lines in the signin page

--- public/modules/e2e/home.login.Spec.js
+++ public/modules/e2e/home.login.Spec.js
@@ -14,6 +14,7 @@ describe('Andonation Homepage', function() {
   it('should show the signin button to unathenticated users', function() {
     expect(signInButton.isDisplayed()).toBe(true);
   });
+
   it('should not show myAndonation button to unauthenticated users', function() {
     expect(element(by.id('myCampaign')).isDisplayed()).toBe(false);
     expect(element(by.id('myAndonation')).isDisplayed()).toBe(false);
@@ -36,5 +37,19 @@ describe('Andonation Homepage', function() {
   it('should show the signin button after logging out', function() {
     expect(signInButton.isDisplayed()).toBe(true);
   });
+  
+  // it('should login with another credentials', function(){
+  //   helper.logoutifLoggedIn();
+  //   helper.login('mirabel.ekwenuogo@andela.co', 'divinemimi');
+  //   var myAndonation = browser.driver.findElement(by.id('myAndonation'));
+  //   expect(myAndonation.getText()).toBeDefined();
+    
+  //   var myCampaign = browser.driver.findElement(by.id('myCampaign'));
+  //   expect(myCampaign.getText()).toBeDefined();
+  //   expect(signInButton.isDisplayed()).toBe(false);
+  //   helper.logoutifLoggedIn();
+
+  // });
+
 });
 
