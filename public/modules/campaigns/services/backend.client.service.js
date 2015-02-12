070ad9d9147f7d774fd545f1c8a1424253c67add
Abimbola Idowu <abimbola.idowu@andela.co>
2015-02-12 13:02:10 +0100

    add campaign controller created

---
+++ public/modules/campaigns/services/backend.client.service.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module('campaign').factory('backendService', ['$http', function($http) {
+
+  //creates a campaign
+  var addCampaign = function(campaignData) {
+    return $http.post('/campaign/add', campaignData);
+  };
+
+  return {
+    addCampaign: addCampaign
+  };
+}]);
