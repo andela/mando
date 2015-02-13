34449c12ae79bc92b08685901eaad9041bc7b2e6
Abimbola Idowu <abimbola.idowu@andela.co>
2015-02-13 14:57:06 +0100

    edited test env to work on circleci

--- gruntfile.js
+++ gruntfile.js
@@ -186,7 +186,7 @@ module.exports = function(grunt) {
   grunt.registerTask('build', ['lint', 'loadConfig', 'ngAnnotate', 'uglify', 'cssmin']);
 
   // Test task.
-  grunt.registerTask('test', ['env:test', 'mochaTest', 'karma:unit']);
+  grunt.registerTask('test', ['env:test', 'mochaTest', 'karma:unit', 'protractor:all']);
 
   //TEST Protractor
   grunt.registerTask('test-protractor', ['env:test', 'protractor:all']);
