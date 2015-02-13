22403e4fd0fcad12ff3d363c2c63f4be60677d23
Abimbola Idowu <abimbola.idowu@andela.co>
2015-02-13 07:13:50 +0100

    fixed bug with multiple google sessions by running different driver instances

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,9 +1,14 @@
-// conf.js
+//config file for protractor
 exports.config = {
- // seleniumAddress: 'http://localhost:4444/wd/hub',
-//  specs: ['home.login.spec.js'],
 
-  baseUrl: 'http://localhost:3000',   
-   
-  specs: ['public/modules/e2e/*Spec.js']   
+  baseUrl: 'http://localhost:3000',
+  //multicapabilities to run different instances of the browser
+  multiCapabilities: [{
+    'browserName': 'chrome',
+    'specs': ['public/modules/e2e/*Spec.js'],
+    'exclude': ['public/modules/e2e/e2e.authentication.Spec.js']
+  }, {
+    'browserName': 'chrome',
+    'specs': ['public/modules/e2e/e2e.authentication.Spec.js']
+  }]
  };
