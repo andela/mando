aa1bb1c96617b19bf72dbdc7c89c717b090e1ae2
Abimbola Idowu <abimbola.idowu@andela.co>
2015-02-13 14:57:06 +0100

    edited test env to work on circleci

--- config/env/test.js
+++ config/env/test.js
@@ -2,13 +2,13 @@
 
 module.exports = {
 	db: 'mongodb://localhost/andonation-test',
-	port: 3001,
+	port: 3000,
 	app: {
-		title: 'Andonation - Test Environment'
+		title: 'Andonation'
 	},
 	google: {
-		clientID: process.env.GOOGLE_ID || 'APP_ID',
-		clientSecret: process.env.GOOGLE_SECRET || 'APP_SECRET',
+		clientID: process.env.GOOGLE_ID || '533269567434-r3uvhfndb2lenmd48cehv9usupqe6mhe.apps.googleusercontent.com',
+		clientSecret: process.env.GOOGLE_SECRET || 'xAPefjMdlMmjWM-gg55xaUam',
 		callbackURL: '/auth/google/callback'
 	}
 };
