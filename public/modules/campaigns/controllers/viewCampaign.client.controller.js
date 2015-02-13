bf79cb50d53bce42ee4386efa97192d707360c66
Ekwenugo Mirabel <mirabel.ekwenugo@andela.co>
2015-02-13 17:29:04 +0100

    split youtube url to obtain ID and embed it, allow only youtube's url by restricting the sceDelegateProvider, binding data to the UI from the view markup

--- public/modules/campaigns/controllers/viewCampaign.client.controller.js
+++ public/modules/campaigns/controllers/viewCampaign.client.controller.js
@@ -1,22 +1,27 @@
 'use strict';
 
-angular.module('campaign').controller('viewCampaignCtrl', ['$scope', 'backendService', '$location', 'Authentication',
-function($scope, backendService, $location, Authentication) {
+angular.module('campaign').controller('viewCampaignCtrl', ['$scope', 'backendService', '$location', 'Authentication', '$stateParams',
+function($scope, backendService, $location, Authentication, $stateParams) {
   $scope.authentication = Authentication;
-    $scope.campaign = {};
-
+    $scope.campaign = {
+      _id: $stateParams.campaignid
+    };
     if (!$scope.authentication.user) {
       $location.path('/');
     }
-    $scope.viewCampaign = function() {
-      backendService.viewCampaign($scope.campaign)
+      backendService.getCampaign($scope.campaign)
       .success(function(data, status, header, config) {
+
+          var youtube = data.youtubeUrl.split('watch?v=');
+          if(youtube.length > 1){
+            data.youtubeId = 'http://www.youtube.com/embed/'+youtube[1];
+          }
+          $scope.campaign = data;
           console.log(data);
-          $location.path('/campaign/'+ data._id);
+          //$location.path('/campaign/'+ data._id);
         })
         .error(function(error, status, header, config) {
           console.log(error);
         });
-    };
-  }
+    }
 ]);
