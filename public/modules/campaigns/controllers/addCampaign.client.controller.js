d66f7339983c214811a3f1f1d05f3cf504e80c01
Ekwenugo Mirabel <mirabel.ekwenugo@andela.co>
2015-02-18 13:57:21 +0100

    youtube invalid error messages

--- public/modules/campaigns/controllers/addCampaign.client.controller.js
+++ public/modules/campaigns/controllers/addCampaign.client.controller.js
@@ -23,11 +23,11 @@ angular.module('campaign').controller('addCampaignCtrl', ['$scope', 'backendServ
          youtubeId = youtube[1];
       }
 
-      backendService.checkYouTubeUrl(youtubeId)
-      .success(function (result) {
-        $scope.youtubeError = '';
+      // backendService.checkYouTubeUrl(youtubeId)
+      // .success(function (result) {
+      //   $scope.youtubeError = '';
         // Add campaign in youtube url is valid
-        console.log(result);
+        // console.log(result);
         backendService.addCampaign($scope.campaign)
         .success(function(data, status, header, config) {
           console.log(data);
@@ -36,12 +36,31 @@ angular.module('campaign').controller('addCampaignCtrl', ['$scope', 'backendServ
         .error(function(error, status, header, config) {
           console.log(error);
         });
-      })
-      .error(function (error){
-        $scope.youtubeError = error;
+      // });
+      // .error(function (error){
+      //   $scope.youtubeError = error;
         //console.log('Invalid YouTube video');
-      });
+      // });
+    };
 
+    $scope.validateYoutubeUrl = function (url) {
+      // console.log('checking');youtubeError
+      var youtube = $scope.campaign.youtubeUrl.split('watch?v=');
+      var youtubeId = null;
+      if(youtube.length > 1){
+         youtubeId = youtube[1];
+      }
+      backendService.checkYouTubeUrl(youtubeId)
+        .success(function (result) {
+          $scope.youtubeError = '';
+          // Add campaign in youtube url is valid
+          console.log(result);
+        })
+        .error(function (error){
+          $scope.youtubeError = error;
+          console.log(error);
+          //console.log('Invalid YouTube video');
+        });
     };
     //to open the calendar
     $scope.open = function($event) {
