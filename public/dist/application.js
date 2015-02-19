cae802a32567b9f914f41d3f0f73bcc650b92a4c
Abimbola Idowu <abimbola.idowu@andela.co>
2015-02-19 04:33:20 +0100

    fixed bug with application build on heroku

--- public/dist/application.js
+++ public/dist/application.js
@@ -51,133 +51,6 @@ ApplicationConfiguration.registerModule('campaign');
 ApplicationConfiguration.registerModule('core');
 'use strict';
 
-var helper = require('./helpers');
-
-describe('Andonation Authentication', function() {
-  beforeEach(function() {
-    helper.loadApp('/');
-  });
-  var signInButton = element(by.id('signIn'));
-  // var googleSignIn = element(by.id('googleSignIn'));
-  
-
-
-
-  it('should accept only emails with andela.co domain name', function() {
-    
-    helper.logoutifLoggedIn();
-    //log in
-    helper.login('mirabelkoso@gmail.com', 'divinemimi');
-
-
-    var myAndonation = browser.driver.findElement(by.id('myAndonation'));
-    expect(myAndonation.isDisplayed()).toBe(false);
-    var myCampaign = browser.driver.findElement(by.id('myCampaign'));
-    expect(myCampaign.isDisplayed()).toBe(false);
-    expect(signInButton.isDisplayed()).toBe(true);
-    helper.logoutifLoggedIn();
-  });
-});
-'use strict';
-/* global it describe browser expect */
-var helper = require('./helpers');
-
-describe('Andonation HomePage', function() {
-  it('should have a title', function() {
-    helper.loadApp();
-    expect(browser.getTitle()).toEqual('Andonation');
-  });
-});
-'use strict';
-
-//this file is to factor out test codes to avoid repetition,
-//any code that would be repeat or used more than once should be written here
-var elements = {
-  signInButton: element(by.id('signIn')),
-  signoutButton: element(by.id('signout'))
-};
-var helpers = {
-  //loads the app at the given url
-  loadApp: function(initialUrl) {
-    initialUrl = initialUrl || '/';
-    browser.get(initialUrl);
-  },
-  //login to the app
-  login: function(email, password) {
-    email = email || 'adebayo.maborukoje@andela.co';
-    password = password || 'maborukoje2012'; //find a way to hide user credentials
-
-    elements.signInButton.click();
-
-    var emailInput = browser.driver.findElement(by.id('Email'));
-    emailInput.sendKeys(email);
-    var passwordInput = browser.driver.findElement(by.id('Passwd'));
-    passwordInput.sendKeys(password);  //you should not commit this to VCS
-    var googleSignIn = browser.driver.findElement(by.id('signIn'));
-    googleSignIn.click();
-
-    // we're about to authorize some permissions, but the button isn't enabled for a second
-    browser.driver.sleep(2500);
-
-    var submitApproveAccess = browser.driver.findElement(by.id('submit_approve_access'));
-    submitApproveAccess.click();
-
-    // this Allows Angular to Load
-    browser.driver.sleep(5000);
-  },
-  logoutifLoggedIn: function() {
-    elements.signoutButton.isDisplayed().then(function(isPresent) {
-      if(isPresent) {
-        elements.signoutButton.click();
-      }
-    });
-  }
-};
-
-module.exports = helpers;
-'use strict';
-/* global describe */
-var helper = require('./helpers');
-describe('Andonation Homepage', function() {
-  beforeEach(function() {
-    helper.loadApp('/');
-  });
-  var signInButton = element(by.id('signIn'));
-
-  it('should have a title', function() {
-   expect(browser.getTitle()).toEqual('Andonation');
-  });
-
-  it('should show the signin button to unathenticated users', function() {
-    expect(signInButton.isDisplayed()).toBe(true);
-  });
-  it('should not show myAndonation button to unauthenticated users', function() {
-    expect(element(by.id('myCampaign')).isDisplayed()).toBe(false);
-    expect(element(by.id('myAndonation')).isDisplayed()).toBe(false);
-  });
-
-  it('should display \'My Andonation\' Upon Successful login', function(){
-    //logout any user if logged in
-    helper.logoutifLoggedIn();
-    //log in
-    helper.login();
-
-    var myAndonation = browser.driver.findElement(by.id('myAndonation'));
-    expect(myAndonation.getText()).toBeDefined();
-    
-    var myCampaign = browser.driver.findElement(by.id('myCampaign'));
-    expect(myCampaign.getText()).toBeDefined();
-    expect(signInButton.isDisplayed()).toBe(false);
-    helper.logoutifLoggedIn();
-  });
-  it('should show the signin button after logging out', function() {
-    expect(signInButton.isDisplayed()).toBe(true);
-  });
-});
-
-
-'use strict';
-
 // Use Applicaion configuration module to register a new module
 ApplicationConfiguration.registerModule('users');
 'use strict';
@@ -193,33 +66,73 @@ angular.module('campaign').config(['$stateProvider', '$sceDelegateProvider', fun
       templateUrl: 'modules/campaigns/views/viewCampaign.client.view.html'
     });
 
-    // Add YouTube to resource whitelist so that we can embed YouTube videos
-    $sceDelegateProvider.resourceUrlWhitelist(['http://www.youtube.com/**']);
+    //Add YouTube to resource whitelist so that we can embed YouTube videos
+    $sceDelegateProvider.resourceUrlWhitelist(['**']);
 }]);
+
+//ANGULAR 1.2 HAS A NEW SECURITY POLICY TO BLOCK OR PREVENT HACKERS
+
+
 'use strict';
+
+/*global moment */
+
+
 
 angular.module('campaign').controller('addCampaignCtrl', ['$scope', 'backendService',  '$location','Authentication',
   function($scope, backendService, $location, Authentication) {
     //provides the authentication object
     $scope.authentication = Authentication;
     $scope.campaign = {};
-    $scope.minDate = moment().format();
-    $scope.maxDate = moment().format();
+
+    //using moment.js to manipulate date
+    $scope.minDate = moment().add(1, 'days');
+    $scope.maxDate = moment().add(30, 'days');
+
     // console.log(1, backendService);
+
    // if unauthenticated, go to home
     if (!$scope.authentication.user) {
       $location.path('/');
     }
+
     $scope.addCampaign = function() {
-      backendService.addCampaign($scope.campaign)
+      var youtube = $scope.campaign.youtubeUrl.split('watch?v=');
+      var youtubeId = null;
+      if(youtube.length > 1){
+         youtubeId = youtube[1];
+      }
+
+        backendService.addCampaign($scope.campaign)
         .success(function(data, status, header, config) {
-          console.log(data);
           $location.path('/campaign/'+ data._id);
         })
         .error(function(error, status, header, config) {
-          console.log(error);
+          $scope.error = error;
         });
     };
+
+    $scope.validateYoutubeUrl = function (url) {
+      // console.log('checking');youtubeError
+      var youtube = $scope.campaign.youtubeUrl.split('watch?v=');
+      var youtubeId = null;
+      if(youtube.length > 1){
+         youtubeId = youtube[1];
+      }
+      backendService.checkYouTubeUrl(youtubeId)
+        .success(function (result) {
+          $scope.youtubeError = '';
+          // Add campaign in youtube url is valid
+          console.log(result);
+        })
+        .error(function (error){
+          $scope.youtubeError = error;
+          console.log(error);
+          //console.log('Invalid YouTube video');
+        });
+    };
+
+    //to open the calendar
     $scope.open = function($event) {
       $event.preventDefault();
       $event.stopPropagation();
@@ -244,7 +157,7 @@ function($scope, backendService, $location, Authentication, $stateParams) {
 
           var youtube = data.youtubeUrl.split('watch?v=');
           if(youtube.length > 1){
-            data.youtubeId = 'http://www.youtube.com/embed/'+youtube[1];
+             data.youtubeId = 'http://www.youtube.com/embed/'+youtube[1];
           }
           $scope.campaign = data;
           console.log(data);
@@ -268,9 +181,14 @@ angular.module('campaign').factory('backendService', ['$http', function($http) {
     return $http.get('/campaign/'+campaignData._id);
   };
 
+  var checkYouTubeUrl = function(videoId) {
+    return $http.get('http://gdata.youtube.com/feeds/api/videos/'+videoId+'?alt=json');
+  };
+
   return {
     addCampaign: addCampaign,
-    getCampaign: getCampaign
+    getCampaign: getCampaign,
+    checkYouTubeUrl: checkYouTubeUrl
   };
 }]);
 'use strict';
