a8375215106e0c74ca26d1d195b4046ed446e376
Adebayo Maborukoje <adebayo@Andelas-MacBook-Pro-3.local>
2015-02-20 22:28:17 +0100

    fixed the toggle down button to display menu items

--- public/modules/core/controllers/header.client.controller.js
+++ public/modules/core/controllers/header.client.controller.js
@@ -1,7 +1,16 @@
 'use strict';
 
-angular.module('core').controller('HeaderController', ['$scope', 'Authentication',
+angular.module('core').controller('HeaderController', ['$scope', 'Authentication', 
   function($scope, Authentication) {
     $scope.authentication = Authentication;
+    $scope.isCollapsed = false;
+    $scope.toggleCollapsibleMenu = function() {
+        $scope.isCollapsed = !$scope.isCollapsed;
+    };
+
+    // Collapsing the menu after navigation
+    $scope.$on('$stateChangeSuccess', function() {
+        $scope.isCollapsed = false;
+    });
   }
 ]);
