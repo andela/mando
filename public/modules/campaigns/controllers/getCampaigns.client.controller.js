d56f6e9b92356d1c45848518f8eeb1985bc53986
Ekwenugo Mirabel <mirabel.ekwenugo@andela.co>
2015-02-20 17:08:15 +0100

    binded all created campaigns to the get all campaigns view

--- public/modules/campaigns/controllers/getCampaigns.client.controller.js
+++ public/modules/campaigns/controllers/getCampaigns.client.controller.js
@@ -2,13 +2,16 @@
 
 angular.module('campaign').controller('getCampaignsCtrl', ['$scope', 'backendService', '$location', 'Authentication', '$stateParams',
 function($scope, backendService, $location, Authentication, $stateParams) {
+  $scope.myCampaigns ={};
   $scope.authentication = Authentication;
     if (!$scope.authentication.user) {
       $location.path('/');
     }
 
-    backendService.getUserCampaigns($scope.authentication.user).success(function(result) {
-      console.log(1, result);
+    backendService.getUserCampaigns($scope.authentication.user).success(function(myCampaigns) {
+      $scope.myCampaigns = myCampaigns;
+      console.log(1, myCampaigns);
+      console.log(2, $scope.myCampaigns);
     });
 
   }]);
