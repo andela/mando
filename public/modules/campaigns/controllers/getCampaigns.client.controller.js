600087efe286337072dbac884b669535ce48227e
Ekwenugo Mirabel <mirabel.ekwenugo@andela.co>
2015-02-20 21:05:42 +0100

    toggle campaign list between 4 and the campaign length

--- public/modules/campaigns/controllers/getCampaigns.client.controller.js
+++ public/modules/campaigns/controllers/getCampaigns.client.controller.js
@@ -2,16 +2,27 @@
 
 angular.module('campaign').controller('getCampaignsCtrl', ['$scope', 'backendService', '$location', 'Authentication', '$stateParams',
 function($scope, backendService, $location, Authentication, $stateParams) {
-  $scope.myCampaigns ={};
+  $scope.myCampaigns    = [];
   $scope.authentication = Authentication;
-    if (!$scope.authentication.user) {
-      $location.path('/');
-    }
 
-    backendService.getUserCampaigns($scope.authentication.user).success(function(myCampaigns) {
-      $scope.myCampaigns = myCampaigns;
-      console.log(1, myCampaigns);
-      console.log(2, $scope.myCampaigns);
-    });
+  if (!$scope.authentication.user) {
+    $location.path('/');
+  }
 
-  }]);
+  // using the backend service to get campaign data from the back end
+  backendService.getUserCampaigns($scope.authentication.user).success(function(myCampaigns) {
+    $scope.myCampaigns = myCampaigns;
+  });
+
+  // function to click the show more button on getMoreCampaigns page
+  $scope.limit = 4;
+  $scope.increment = function() {
+    var campaignLength = $scope.myCampaigns.length;
+    $scope.limit = campaignLength;
+  };
+
+  $scope.decrement = function() {
+    $scope.limit = 4;
+  };
+
+}]);
