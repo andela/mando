93d9cd14f54ed6af706b01f940a27231dc0680d7
Abimbola Idowu <abimbola.idowu@andela.co>
2015-02-21 20:14:21 +0100

    added unit test for userCampaigns controllers

--- public/modules/campaigns/tests/userCampaigns.client.controller.test.js
+++ public/modules/campaigns/tests/userCampaigns.client.controller.test.js
@@ -1,25 +1,29 @@
 'use strict';
 
-describe('userCampaignCtrl', function() {
-  var userCampaignCtrl, userid, scope, $httpBackend, $stateParams;
+describe('userCampaignsCtrl', function() {
+  var userCampaignsCtrl, scope, userid, $httpBackend;
   //Loading the main Application module
   beforeEach(module(ApplicationConfiguration.applicationModuleName));
-  beforeEach(inject(function($controller, $rootScope, _$location_, _$stateParams_, _$httpBackend_) {
-    scope = $rootScope.new();
-    $stateParams = _$stateParams_;
+  beforeEach(inject(function($controller, $rootScope, _$httpBackend_, Authentication) {
+    scope = $rootScope.$new();
+    Authentication.user = {
+        _id: '54da2257568f9cfd6d2dba2f'
+    };
+    userid = '54da2257568f9cfd6d2dba2f';
     $httpBackend = _$httpBackend_;
-    userid = '54da2257568f9cfd6d2dba2f'
-
-    userCampaignCtrl = $controller('userCampaignCtrl', {
+    userCampaignsCtrl = $controller('userCampaignsCtrl', {
       $scope: scope,
     });
   }));
   it('should return all campaigns created by a user', function() {
-    $httpBackend.when('GET', '/campaigns/' + userid).respond(200, [
-      {
-    "_id" : ObjectId("54e4b7eba241567dbbec7122"),
-    "lastModifiedBy" : "54da2257568f9cfd6d2dba2f",
-}
-      ])
+    $httpBackend.when('GET', '/campaigns/' + userid).respond(200, {
+        '_id' : '54e4b7eba241567dbbec7122',
+        'lastModifiedBy' : '54da2257568f9cfd6d2dba2f',
+      }
+    );
+    console.log(scope.myCampaigns);
+    $httpBackend.flush();
+    console.log(scope.myCampaigns);
+    expect(typeof scope.myCampaigns).toBe('object');
   });
 });
