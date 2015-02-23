bd6c3213743919cb589b2bf29616fd8619da7424
andela-amaborukoje <adebayo.maborukoje@andela.co>
2015-02-23 18:20:36 +0100

    uncommented the user authentication route at the backend

--- app/routes/campaign.server.routes.js
+++ app/routes/campaign.server.routes.js
@@ -7,8 +7,8 @@ var express = require('express'),
 
 
 module.exports = function(app) {
-  // app.route('/campaign/*').all(users.requiresLogin);
-  // app.route('/campaigns/*').all(users.requiresLogin);
+  app.route('/campaign/*').all(users.requiresLogin);
+  app.route('/campaigns/*').all(users.requiresLogin);
   app.route('/campaign/add').post(campaigns.createCampaign);
   app.route('/campaign/:campaignId').get(campaigns.getCampaign);
   app.route('/campaign/:campaignId/edit').put(campaigns.updateCampaign);
