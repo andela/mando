bf09e6417d4d26c99ae4d7f2fe2477f1c565265c
Ekwenugo Mirabel <mirabel.ekwenugo@andela.co>
2015-02-23 19:43:32 +0100

    created back end server route to delete a campaign

--- app/routes/campaign.server.routes.js
+++ app/routes/campaign.server.routes.js
@@ -11,5 +11,6 @@ module.exports = function(app) {
   app.route('/campaign/add').post(campaigns.createCampaign);
   app.route('/campaign/:campaignId').get(campaigns.getCampaign);
   app.route('/campaign/:campaignId/edit').put(campaigns.updateCampaign);
+  app.route('/campaign/:campaignId').delete(campaigns.deleteCampaign);
   app.route('/campaigns/:userId').get(campaigns.getCampaigns);
 };
