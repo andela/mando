be3b10d9d8f681aa5f22962ec33cb2517bdb0aa9
andela-amaborukoje <adebayo.maborukoje@andela.co>
2015-02-23 13:04:45 +0100

    added new file for the edit campaign pages

--- public/modules/campaigns/controllers/editCampaign.client.controller.js
+++ public/modules/campaigns/controllers/editCampaign.client.controller.js
@@ -1,15 +1,32 @@
 'use strict';
 
-angular.module('campaign').controller('editCampaignCtrl', ['$scope','backendService', '$location,', 'Authentication', function ($scope, backendService, $location, Authentication) {
+angular.module('campaign').controller('editCampaignCtrl', ['$scope','backendService', '$location', 'Authentication','$stateParams', function ($scope, backendService, $location, Authentication, $stateParams) {
     $scope.Authentication = Authentication;
-    
+     $scope.campaign = {
+        _id: $stateParams.campaignid
+     };
+   
     //route unauhenticated user  to the camapaign view page goes
     if(!$scope.Authentication.user){
 
-    $location.path('/campaign/:campaignid');
+    $location.path('/');
     }
+    
+    backendService.getCampaign($scope.campaign)
+        .success(function(data, status){
+            $scope.campaign = data;
+            $scope.campaign.youtubeUrl =  'https://www.youtube.com/watch?v='+data.youtubeUrl;
+        }).error(function(err){
+        console.log(err);
+    });
 
-
+    $scope.editCampaign =function(){
+        backendService.editCampaign().success(function(data, status){
+        $location.path('/campaign/' + data._id);
+        }).error(function(err){
+        $scope.error = err;
+        });
+    };
 
 }
 ]);
