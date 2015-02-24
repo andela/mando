ba7a37efd5f41c50b5f559cd343ce94079396fe4
Ekwenugo Mirabel <mirabel.ekwenugo@andela.co>
2015-02-24 13:19:19 +0100

    added toastr.js and toastr.css to the config

--- config/env/all.js
+++ config/env/all.js
@@ -27,7 +27,8 @@ module.exports = {
 				'public/lib/angular-ui-utils/ui-utils.js',
 				'public/lib/angular-bootstrap/ui-bootstrap-tpls.js',
 				'public/lib/moment/moment.js',
-				'public/lib/angular-youtube-mb/src/angular-youtube-embed.js'
+				'public/lib/angular-youtube-mb/src/angular-youtube-embed.js',
+				'public/lib/toastr/toastr.css'
 			]
 		},
 		css: [
@@ -37,7 +38,8 @@ module.exports = {
 			'public/config.js',
 			'public/application.js',
 			'public/modules/*/*.js',
-			'public/modules/*/*[!tests]*/*.js'
+			'public/modules/*/*[!tests]*/*.js',
+			'public/lib/toastr/toastr.js'
 		],
 		tests: [
 			'public/lib/angular-mocks/angular-mocks.js',
