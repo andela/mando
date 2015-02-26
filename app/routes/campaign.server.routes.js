ff91bd89d7adf6d09f4f78c6ff614617d88dc7b9
Abimbola Idowu <abimbola.idowu@andela.co>
2015-02-26 21:30:59 +0100

    server routes created to get all routes from the server

--- app/routes/campaign.server.routes.js
+++ app/routes/campaign.server.routes.js
@@ -11,5 +11,6 @@ module.exports = function(app) {
   app.route('/campaign/add').post(campaigns.createCampaign);
   app.route('/campaign/:campaignId').get(campaigns.getCampaign);
   app.route('/campaign/:campaignId/edit').put(campaigns.updateCampaign);
-  app.route('/campaigns/:userId').get(campaigns.getCampaigns);
+  app.route('/campaigns/:userId').get(campaigns.getUserCampaigns);
+  app.route('/campaigns').get(campaigns.getCampaigns);
 };
