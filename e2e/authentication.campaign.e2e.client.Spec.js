ea47405f7b6fa2c1525c4a7ca4ecb4a41fee05c1
Ekwenugo Mirabel <mirabel.ekwenugo@andela.co>
2015-02-27 16:45:54 +0100

    removed unwanted lines of code

--- e2e/authentication.campaign.e2e.client.Spec.js
+++ e2e/authentication.campaign.e2e.client.Spec.js
@@ -22,32 +22,5 @@ describe('Andonation Authentication', function() {
     expect(signInButton.isDisplayed()).toBe(true);
     helper.logoutifLoggedIn();
   });
-  // it('should not show the user campaigns table', function() {
-  //     browser.sleep(2000);
-  //     var login = function(email, password) {
-  //   email = 'mirabel.ekwenugo@andela.co';
-  //   password = 'divinemimi'; //find a way to hide user credentials
-  //   var signInButton =  element(by.id('signIn'));
-  //   signInButton.click();
 
-  //   var emailInput = browser.driver.findElement(by.id('Email'));
-  //   emailInput.sendKeys(email);
-  //   var passwordInput = browser.driver.findElement(by.id('Passwd'));
-  //   passwordInput.sendKeys(password);  //you should not commit this to VCS
-  //   var googleSignIn = browser.driver.findElement(by.id('signIn'));
-  //   googleSignIn.click();
-
-  //   // we're about to authorize some permissions, but the button isn't enabled for a second
-  //   browser.driver.sleep(2500);
-
-  //   var submitApproveAccess = browser.driver.findElement(by.id('submit_approve_access'));
-  //   submitApproveAccess.click();
-
-  //   // this Allows Angular to Load
-  //   browser.driver.sleep(5000);
-  //     var myAndonation = browser.driver.findElement(by.id('myAndonation'));
-  //     myAndonation.click();
-  //     expect(element(element(by.tagName('table')).isPresent)).toBe(false);
-  //   };
-    // });
 });
