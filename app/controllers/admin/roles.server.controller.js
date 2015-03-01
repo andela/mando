a879587844309cd1af93d43970bd8130ab770cb4
Abimbola Idowu <abimbola.idowu@andela.co>
2015-03-01 13:48:44 +0100

    rewrote addUserRoles controller

--- app/controllers/admin/roles.server.controller.js
+++ app/controllers/admin/roles.server.controller.js
@@ -1,7 +1,9 @@
 'use strict';
 
 var mongoose = require('mongoose'),
+    async = require('async'),
     Role = mongoose.model('Role'),
+    User = mongoose.model('User'),
     UserRole = mongoose.model('UserRole'),
     User = mongoose.model('User'),
     errorHandler = require('../errors.server.controller');
@@ -31,28 +33,51 @@ exports.getRoles = function(req, res) {
   });
 };
 
-//finds a user role id since the role is already defined
-var getRoleId = function(role) {
-  Role.findOne({role: role}, function (err, role) {
-    return role._id;
-  });
-};
-
 //adds a role to a user
 exports.addUserRoles = function(req, res) {
   var userRole = new UserRole(req.body);
   userRole.createdBy = req.user._id;
   userRole.lastModifiedBy = req.user._id;
-  userRole.roleId.push(getRoleId(req.body.role));
   userRole.userId = req.body.user._id;
-  userRole.save(function(err, newUserRole) {
-    if(err){
-      return res.status(400).send({
-        message: errorHandler.getErrorMessage(err)
+
+  async.series([
+    function(cb) {
+      Role.findOne({roleType: req.body.role}, function (err, role) {
+        if(err){
+          return res.status(400).send({
+            message: errorHandler.getErrorMessage(err)
+          });
+        }
+        if(!role) {
+          return res.status(400).send({message: 'Invalid Role Type'});
+        }
+        userRole.roleId.push(role._id);
+        cb(err);
+      });
+    },
+    function(cb) {
+      userRole.save(function(err, newUserRole) {
+        if(err){
+          return res.status(400).send({
+            message: errorHandler.getErrorMessage(err)
+          });
+        }
+        //updates the usel document with the new role added
+        User.findByIdAndUpdate(req.body.user._id, {role: newUserRole._id}, {}, function(err, updatedUser) {
+          //populates the role field in the user document
+          User.populate(updatedUser, {path: 'role'}, function(err, populatedUser) {
+            //updates the roleId field in the userRole document
+            UserRole.populate(populatedUser.role, {path: 'roleId'}, function(err, populatedUserRole) {
+            console.log(1, populatedUser);
+            console.log(2, populatedUserRole);
+            res.json(populatedUser);
+            });
+          });
+        });
+        cb();
       });
     }
-    res.json(newUserRole);
-  });
+  ]);
 };
 
 //get all users as an admin
