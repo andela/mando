246d7c6e5cc2a148c24e2d1036187c121156d832
Abimbola Idowu <abimbola.idowu@andela.co>
2015-03-02 11:48:23 +0100

    added dummy data to call the add roles api

--- public/modules/admin/controllers/users.admin.client.controller.js
+++ public/modules/admin/controllers/users.admin.client.controller.js
@@ -1,6 +1,18 @@
 'use strict';
 
-angular.module('admin').controller('adminUserCtrl', ['$scope', 'adminBackendService', function($scope, adminBackendService) {
+angular.module('admin').controller('adminUserCtrl', ['$scope', 'adminBackendService', 'Authentication', function($scope, adminBackendService, Authentication) {
+  // var role = {};
+  // role.role = 'member';
+  // role.user = {};
+  // role.user._id = '54f2f47b599d4e96a54e5a3d';
+  // adminBackendService.addRolesToUser(role)
+  //   .success(function(data, status, header, config) {
+  //     console.log(data);
+  //     $scope.role = data;
+  //   })
+  //   .error(function(error, status, header, config) {
+  //     console.log(error);
+  //   });
   adminBackendService.getUsers()
     .success(function(data, status, header, config) {
       $scope.users = data;
