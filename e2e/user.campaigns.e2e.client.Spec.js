9f2f4bc03c571a6eb658738cdf05591fc4ab2e83
Abimbola Idowu <abimbola.idowu@andela.co>
2015-03-03 13:45:03 +0100

    added test for invalid url bug

--- e2e/user.campaigns.e2e.client.Spec.js
+++ e2e/user.campaigns.e2e.client.Spec.js
@@ -6,15 +6,24 @@ describe('userCampaigns', function() {
   beforeEach(function() {
     helper.loadApp('/');
   });
+  var myAndonation = element(by.id('myAndonation'));
 
   it('should load user campaign page', function() {
     helper.logoutifLoggedIn();
     var signInButton = element(by.id('signIn'));
     expect(signInButton.isDisplayed()).toBe(true, 'user logged out');
     signInButton.click();
-    var myAndonation = element(by.id('myAndonation'));
+    //var myAndonation = element(by.id('myAndonation'));
     expect(myAndonation.isDisplayed()).toBe(true, 'authenticated user can view \'myAndonation\' page');
     myAndonation.click();
     expect(element(by.tagName('table')).isPresent()).toBe(true);
+  });
+
+  it('should redirect user to home page if invalid user id is entered', function() {
+    myAndonation.click();
+    expect(browser.getCurrentUrl()).toMatch(/[0-9a-fA-F]{24}$/);
+    browser.get('http://localhost:3000/#!/campaigns/54f39eb02e5e56fc7690e2sd');
+    expect(browser.getCurrentUrl()).not.toMatch(/^[0-9a-fA-F]{24}$/);
+    expect(browser.getCurrentUrl()).toBe('http://localhost:3000/#!/');
   });
 });
