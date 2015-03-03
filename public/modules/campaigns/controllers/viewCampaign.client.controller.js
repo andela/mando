ed782dc6ebeab4985cdb8fc47f9be11d7362c65e
andela-amaborukoje <adebayo.maborukoje@andela.co>
2015-03-03 15:50:42 +0100

    removed frontend validation to allow unathenticated user to view campaigns

--- public/modules/campaigns/controllers/viewCampaign.client.controller.js
+++ public/modules/campaigns/controllers/viewCampaign.client.controller.js
@@ -3,9 +3,7 @@
 angular.module('campaign').controller('viewCampaignCtrl', ['$scope','toaster' , 'backendService','$location', 'Authentication', '$stateParams',
 function($scope, toaster, backendService,$location, Authentication, $stateParams) {
   $scope.authentication = Authentication;
-    if (!$scope.authentication.user || !$stateParams.campaignid) {
-      $location.path('/');
-    }
+  
     $scope.campaign = {
       _id: $stateParams.campaignid
     };
