7dc22baa1216437794334508208bd0ac1c42ffba
Ekwenugo Mirabel <mirabel.ekwenugo@andela.co>
2015-03-03 15:45:51 +0100

    added pagination to the home page

--- public/modules/campaigns/controllers/allCampaign.client.controller.js
+++ public/modules/campaigns/controllers/allCampaign.client.controller.js
@@ -1,15 +1,40 @@
 'use strict';
 
-angular.module('campaign').controller('allCampaignCtrl', ['$scope','$location','backendService', function ($scope, $location, backendService) {
+angular.module('campaign').controller('allCampaignCtrl', ['$scope','$log', '$location','backendService', function ($scope, $location, $log, backendService) {
+
+ $scope.totalItems = 1;
 
   backendService.getCampaigns()
   .success(function (data, status, header, config){
-    
     $scope.campaigns = data;
+    $scope.totalItems = data.length;
+    $scope.filterCampaigns();
   })
   .error(function (error, status, header, config){
     console.log(error);
     });
 
+  $scope.filterCampaigns = function () {
+    var begin = (($scope.currentPage - 1) * $scope.itemsPerPage);
+    var end = begin + $scope.itemsPerPage;
+    $scope.startItems = begin+1;
+    // $scope.endItems = end < $scope.totalItems ? end : $scope.totalItems;
+    if(end < $scope.totalItems){
+      $scope.endItems = end;
+    }else{
+      $scope.endItems = $scope.totalItems;
+    }
+
+    $scope.filteredCampaigns = $scope.campaigns.slice(begin, end);
+  };
+
+  $scope.currentPage = 1;
+  $scope.itemsPerPage = 21;
+  $scope.filteredCampaigns = [];
+
+  $scope.pageChanged = function() {
+    console.log(10);
+    $scope.filterCampaigns();
+  };
   }
 ]);
