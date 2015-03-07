7c4f09095e926754a126186939fe8e38b75bfb11
andela-amaborukoje <adebayo.maborukoje@andela.co>
2015-03-07 12:36:09 +0100

    added a client view for banker and also controller

---
+++ public/modules/banker/controllers/allTransaction.client.controller.js
@@ -0,0 +1,9 @@
+'use strict';
+
+angular.module('banker').controller('transactionCtrl',['$scope', function($scope){
+
+
+
+}]);
+
+angular.module('banker').constant('name', 'John doe')
---
+++ public/modules/banker/services/banker.client.constants.js
@@ -0,0 +1,8 @@
+'use strict';
+angular.module('banker').constant('BANKERCONST', {
+ID: 'PNKWmtgMsLoHzB4LhUw4qN',
+APP_KEY: '2lzQysbyNXhPgYxx8pp2vE',
+SECRET: 'CJzZPwRw01thgquyeD6RYc',
+ORG_ID: 'EpXxbhcVpxyC8BH0icuIQF',
+BOOK_ID:'R6WkhSAmw4STDyGHbrrFJL',
+});
