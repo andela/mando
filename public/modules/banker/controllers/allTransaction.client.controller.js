fb2c30395933ac2a394295c12652514247933b95
andela-amaborukoje <adebayo.maborukoje@andela.co>
2015-03-09 20:52:12 +0100

    added controller for banker module

--- public/modules/banker/controllers/allTransaction.client.controller.js
+++ public/modules/banker/controllers/allTransaction.client.controller.js
@@ -1,32 +1,21 @@
 'use strict';
+/*global Subledger*/
+angular.module('banker').controller('transactionCtrl',['$scope','$http','bankerFactory',function($scope, $http, bankerFactory){
 
-angular.module('banker').controller('transactionCtrl',['$scope','$http', function($scope, $http){
 $scope.balance = {
-  all: 1000
+  bank_id: 'mnE22eIutb5SwDH69Ernfx',
+  amount: ''
 };
-    var url = 'https://api.subledger.com/v2/';
-    var org_id = 'EpXxbhcVpxyC8BH0icuIQF';
-    var book_id = 'R6WkhSAmw4STDyGHbrrFJL';
-
-var myBalance = function(){
-  console.log('myBalance');
-  //   var req = {
-  //    method: 'GET',
-  //    url: url+'/org_id/'+org_id+'/book_id/'+book_id,
-  //    headers: {
-  //      'Content-Type': 'application/json',
-  //      'key-id':'2lzQysbyNXhPgYxx8pp2vE',
-  //      'secret-key':'CJzZPwRw01thgquyeD6RYc'
-  //    },
-  //  //  data: {},
-  //  };
-  //  $http(req).success(function(data, error, headers, config){
-  //     console.log(1, data);
-  //      $scope.balance = data;
-  //      return $scope.balance
-  //   }).error(function(data, error, headers, config){
-  //     console.log(2, error);
-  //   });    
-  };
+  bankerFactory.getSystemBalance($scope.balance.bank_id).balance({description: 'USD'}, function(error, apiRes){
+        if (error){
+          console.log(error);
+        }else{
+          console.log(3, apiRes);
+          var amount = parseInt(apiRes.balance.value.amount);
+          console.log(3, amount);
+          //$scope.$digest();
+          $scope.balance.amount = amount;
+        }
+      });
 
 }]);
