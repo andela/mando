61fc3be8f0307d9c12ca1817ebf37e2060d45e83
andela-amaborukoje <adebayo.maborukoje@andela.co>
2015-03-09 20:55:38 +0100

    banker can see system balance on my andonation page and on the bankers page

--- public/modules/banker/services/banker.client.services.js
+++ public/modules/banker/services/banker.client.services.js
@@ -1,44 +1,18 @@
-// 'use strict';
-// angular.module('banker').factory('bankerFactory', [ 'BANKERCONST',
-//   '$http', function(BANKERCONST, $http) {
+'use strict';
+/*global Subledger*/
 
-//     var url = 'https://api.subledger.com/v2/';
-//     var org_id = 'EpXxbhcVpxyC8BH0icuIQF';
-//     var book_id = 'R6WkhSAmw4STDyGHbrrFJL';
+angular.module('banker').factory('bankerFactory', [ function(){
+  var subledger = new Subledger();
+  var identity_id = 'PNKWmtgMsLoHzB4LhUw4qN';
+  var org_id = 'EpXxbhcVpxyC8BH0icuIQF';
+  var book_id = 'R6WkhSAmw4STDyGHbrrFJL';
+  subledger.setCredentials('2lzQysbyNXhPgYxx8pp2vE','CJzZPwRw01thgquyeD6RYc');
+  
+    var getSystemBalance = function (account_id){
+      return subledger.organization(org_id).book(book_id).account(account_id);
 
-//   //Get the Balance of the bankker from his subledger account.
-//   var getBankBalance = function(){
-//     var req = {
-//      method: 'GET',
-//      url: url+'/org_id/'+org_id+'/book_id/'+book_id,
-//      headers: {
-//        'Content-Type': 'application/json',
-//        'key-id':'2lzQysbyNXhPgYxx8pp2vE',
-//        'secret-key':'CJzZPwRw01thgquyeD6RYc'
-//      },
-//    //  data: {},
-//    };
-
-
-//     return $http(req).success(function(data, error, headers, config){
-//       console.log(1, data);
-//       return data;
-
-//     }).error(function(data, error, headers, config){
-//       console.log(2, error);
-//     });    
-//   };
-//    //the Http request for the  entire Organisation banker with a get request
-
-//   // var getUniqueBankerTransaction = function(bankderId){
-//   //   return; // the http return transaction details/history for the banker 
-//   // };
-
-// }
-// ]);
-
-// // // send some combined key+secret combination
-// // // use the response and modify it in some way
-// // // attach that key to the API request that you want to make
-
-// // // page -> navigation bar + content -> campaign + if banker show bank module
+    };
+    return {
+      getSystemBalance: getSystemBalance
+    };
+  }]);
--- public/modules/campaigns/controllers/userCampaigns.client.controller.js
+++ public/modules/campaigns/controllers/userCampaigns.client.controller.js
@@ -1,8 +1,12 @@
 'use strict';
-angular.module('campaign').controller('userCampaignsCtrl', ['$scope', 'backendService', '$location', 'Authentication', '$stateParams', 'lodash',
-function($scope, backendService, $location, Authentication, $stateParams, lodash) {
+
+angular.module('campaign').controller('userCampaignsCtrl', ['$scope', 'backendService', '$location', 'bankerFactory','Authentication', '$stateParams', 'lodash',
+function($scope, backendService, $location, bankerFactory, Authentication, $stateParams, lodash) {
+
+///bankerFactory  add this to the dependency to get the system balance
 
   $scope.myCampaigns    = [];
+  $scope.balance = {};
   $scope.authentication = Authentication;
 
   if (!$scope.authentication.user) {
@@ -24,7 +28,23 @@ function($scope, backendService, $location, Authentication, $stateParams, lodash
       $location.path('/');
 
     });
-
+    //if role = banker use the banker id here else you the user's id$scope.authentication.user.account_id
+    var account_id = 'mnE22eIutb5SwDH69Ernfx';
+    console.log('account is '+ $scope.authentication.user.account_id);
+//    $scope.systemBalance = function(account_id){ 
+  //    console.log('inside'+account_id);
+      bankerFactory.getSystemBalance(account_id).balance({description: 'USD'}, function(error, apiRes){
+        if (error){
+          console.log(error);
+        }else{
+          console.log(3, apiRes);
+          var amount = parseInt(apiRes.balance.value.amount);
+          console.log('here', amount);
+          $scope.balance.amount = amount;
+          $scope.$digest();
+        }
+      });
+ // };
   // function to click the show more button on getMoreCampaigns page
   $scope.limit = 4;
   $scope.increment = function() {
