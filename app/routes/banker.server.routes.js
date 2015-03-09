200c15819bd82b23bc1c9a1c8e28617a6ec134f9
andela-amaborukoje <adebayo.maborukoje@andela.co>
2015-03-09 23:25:01 +0100

    clean up the code to remove all console log and unwanted lines/whitespaces

--- app/routes/banker.server.routes.js
+++ app/routes/banker.server.routes.js
@@ -5,9 +5,7 @@ module.exports = function(app){
   app.route('/identity').get(banker.getIdentity);
   app.route('/accounts').get(banker.getAllAccounts);
   app.route('/accounts/:account_id').get(banker.getUniqueAccount);
-
   // The below route will be removed and moved to delete my account setting in the future as accounts can only be archived a user suspends his/her accounts.
   app.route('/account').get(banker.ArchiveAccount);
-
 
 };
