df871bf8de3da47ecbe78ac6591c2a8a89d0f195
andela-amaborukoje <adebayo.maborukoje@andela.co>
2015-03-10 15:58:10 +0100

    banker can withdraw money from the system

--- public/modules/banker/controllers/allTransaction.client.controller.js
+++ public/modules/banker/controllers/allTransaction.client.controller.js
@@ -1,94 +1,116 @@
 'use strict';
 /*global Subledger*/
-angular.module('banker').controller('transactionCtrl',['$scope','$http','toaster','bankerFactory',function($scope, $http,toaster, bankerFactory){
-$scope.reports = [];
-$scope.balance = {
-  bank_id: 'mnE22eIutb5SwDH69Ernfx',
-  amount: ''
-};
-var date = new Date().toISOString();
+angular.module('banker').controller('transactionCtrl',['$scope','$http','toaster','$modal','bankerFactory',function($scope, $http,toaster, $modal, bankerFactory){
+  $scope.reports = [];
+  $scope.withdrawal= {};
+  $scope.balance = {
+    bank_id: '6HNEAjoyxWtXjVXD2TyZqE',
+    amount: ''
+  };
+  var date = new Date().toISOString();
 
 //console.log('THE DATE IS'+ date);
-  bankerFactory.getSystemBalance($scope.balance.bank_id).balance({description: 'USD', at:date}, function(error, apiRes){
-        if (error){
-           toaster.pop('error', 'An Error Occurred'+ error);
-           return;
-        }else{
-          var amount = parseInt(apiRes.balance.value.amount);
-          $scope.balance.amount = amount;
-          $scope.$digest();
-        }
-      });
+bankerFactory.getSystemBalance($scope.balance.bank_id).balance({description: 'USD', at:date}, function(error, apiRes){
+  if (error){
+   toaster.pop('error', 'An Error Occurred'+ error);
+   return;
+ }else{
+  var amount = parseInt(apiRes.balance.value.amount);
+  $scope.balance.amount = amount;
+  $scope.$digest();
+}
+});
 
-// //debtor and creditor 
-// var InitiateTransaction = function(){
-//   var params = {
-//   'effective_at': new Date().toISOString(),
-//   'description': $scope.description,
-//   'reference': 'http://andela.co',
-//   'lines': [
-//       {
-//       'account': $scope.account_id, //debtor
-//       'description': $scope.descrition,
-//       'reference': 'http://andela.co',
-//        'value': {
-//          'type': 'debit',
-//          'amount': $scope.amount
-//          }
-//       },
-//      {
-//       'account': $scope.account_id, //creditor
-//       'description':$scope.description,
-//       'reference': 'http://andela.co',
-//        'value': {
-//          'type': 'credit',
-//          'amount': $scope.amount
-//         }
-//       }
-//     ]
-//   };
-  
-
-//   bankerFactory.createAndPostTransaction().createAndPost({params:params}, function(error, apiRes){
-//     if(error){
-//       return error;
-//     } else {
-//       console.log('createAndPost'+ apiRes);
-//     }
-//   });
-
-// };
+$scope.withdrawFromBank = function (){
+ var amount = $scope.withdrawal.amount;
+ console.log('here', amount);
+  bankerFactory.createAndPostTransaction().createAndPost({
+    'effective_at': new Date().toISOString(),
+    'description': 'withdrawal',
+    'reference': 'http://andonation-mando.herokuapp.com/bank',
+    'lines': [
+    {
+      'account': $scope.balance.bank_id,
+      'description': 'cash withdrawal',
+      'reference':  'http://andonation-mando.herokuapp.com/bank',
+      'value': {
+        'type': 'debit',
+        'amount': amount
+      }
+    },
+    // {$scope.system_balance_id
+    {
+      'account': 'E8GtyKhrPjSduG8aHSUusc',
+      'description': 'cash deposit',
+      'reference':  'http://andonation-mando.herokuapp.com/bank',
+      'value': {
+        'type': 'credit',
+        'amount': amount
+      }
+    }
+    ]
+  }, function (error, apiRes){
+    if(error){
+      console.log(error);
+    }
+    else{
+      //toaster.pop('success', amount +'was  Withdrawn Succesfully');
+    //$scope.digest($scope.balance);
+    $scope.$digest();
+  }
+});
+};
   //get All lines of transaction
   bankerFactory.getJournalReports($scope.balance.bank_id).get({
     'description': 'USD',
     'action': 'before',
     'effective_at': new Date().toISOString()
   }, function (error, apiRes){
-      if(error){
-          return error;
-      }else {
+    if(error){
+      return error;
+    }else {
+     // toaster.pop('success', 'Loading Transaction Details');
       $scope.journal = apiRes.posted_lines;
-      toaster.pop('success', 'Loading Transaction Details');
       $scope.$digest();
-      }
+    }
   });
 
   //get All banking Reports 
-    bankerFactory.getReports().get({
-      'description': 'USD', 
-      'state': 'active',
-      'action': 'before',
+  bankerFactory.getReports().get({
+    'description': 'USD', 
+    'state': 'active',
+    'action': 'before',
 
-    },function (error,apiRes){
-      if(error){
-        return error;
-      }else {
-        $scope.reports = apiRes;
-        $scope.$digest();
-      }
+  },function (error,apiRes){
+    if(error){
+      return error;
+    }else {
+      $scope.reports = apiRes;
+      $scope.$digest();
+    }
     
-    });
+  });
 
-  //  bankerFactory
+  //OPEN MODAL WINDOW
+  
+  // $scope.openModal = function(size){
+  //   var modalInstance = $modal.open({
+  //     templateUrl: 'modules/banker/views/support.modal.view.html',
+  //     controller : function ($scope, $modalInstance){
+  //   //  $scope.authentication = Authentication;
 
-}]);
+  //     $scope.ok = function () {
+  //       $modalInstance.close($scope.amount);
+  //     };
+  //     $scope.cancel = function () {
+  //       $modalInstance.dismiss('cancel');
+  //     };
+  //     },
+  //     size: size,
+  //     });
+  //     modalInstance.result().then(function () {
+  //     console.log();
+
+  //   });
+  //  };
+ }]);
