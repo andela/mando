af314e9dc91c2886552c161f2e1fa0e8aa87c0ea
andela-amaborukoje <adebayo.maborukoje@andela.co>
2015-03-13 00:27:50 +0100

    refactoring the editing campaign test to work with the slug url

--- app/routes/banker.server.routes.js
+++ app/routes/banker.server.routes.js
@@ -1,11 +1,16 @@
 'use strict';
 var banker = require('../controllers/banker.server.controller');
+var users = require('../../app/controllers/users.server.controller');
 
 module.exports = function(app){
   app.route('/identity').get(banker.getIdentity);
   app.route('/accounts').get(banker.getAllAccounts);
   app.route('/accounts/:account_id').get(banker.getUniqueAccount);
+<<<<<<< HEAD
   // The below route will be removed and moved to delete my account setting in the future as accounts can only be archived a user suspends his/her accounts.
   app.route('/account').get(banker.ArchiveAccount);
 
+=======
+  app.route('/contants').get(banker.getConstants);
+>>>>>>> refactoring the editing campaign test to work with the slug url
 };
