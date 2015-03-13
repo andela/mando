153872f11daeac4af5356bc699f38635c64592d0
andela-amaborukoje <adebayo.maborukoje@andela.co>
2015-03-13 19:30:22 +0100

    moved the credentials to the backed

--- app/routes/banker.server.routes.js
+++ app/routes/banker.server.routes.js
@@ -6,11 +6,15 @@ module.exports = function(app){
   app.route('/identity').get(banker.getIdentity);
   app.route('/accounts').get(banker.getAllAccounts);
   app.route('/accounts/:account_id').get(banker.getUniqueAccount);
-<<<<<<< HEAD
+// <<<<<<< HEAD
+// <<<<<<< HEAD
   // The below route will be removed and moved to delete my account setting in the future as accounts can only be archived a user suspends his/her accounts.
   app.route('/account').get(banker.ArchiveAccount);
 
-=======
+// =======
   app.route('/contants').get(banker.getConstants);
->>>>>>> refactoring the editing campaign test to work with the slug url
+// >>>>>>> refactoring the editing campaign test to work with the slug url
+// =======
+  app.route('/credentials').get(banker.getConstants);
+// >>>>>>> moved the credentials to the backed
 };
--- public/modules/banker/services/banker.client.services.js
+++ public/modules/banker/services/banker.client.services.js
@@ -1,32 +1,40 @@
 'use strict';
 /*global Subledger*/
 
-angular.module('banker').constant('bankerConstant', {
-  BANK_ID: '6HNEAjoyxWtXjVXD2TyZqE',
-  SYSTEM_ID: 'E8GtyKhrPjSduG8aHSUusc',
-})
-.factory('bankerFactory', [ function(){
+angular.module('banker').factory('bankerFactory', [ '$http', function($http){
   var subledger = new Subledger();
-  var identity_id = 'PNKWmtgMsLoHzB4LhUw4qN';
-  var org_id = 'EpXxbhcVpxyC8BH0icuIQF';
-  var book_id = 'R6WkhSAmw4STDyGHbrrFJL';
-  subledger.setCredentials('2lzQysbyNXhPgYxx8pp2vE','CJzZPwRw01thgquyeD6RYc');
+  var credentials = {};
+
   
-    var getSystemBalance = function (account_id){
+ var setCredentials = function(key_id, secret){
+  subledger.setCredentials(key_id, secret); 
+ };
+    var getSystemBalance = function (org_id, book_id, account_id){
       return subledger.organization(org_id).book(book_id).account(account_id);
     };
 
-    var createAndPostTransaction = function (){
+    var createAndPostTransaction = function (org_id, book_id){
      return subledger.organization(org_id).book(book_id).journalEntry();
     };
 
-    var getJournalReports = function(account_id){
-       return subledger.organization(org_id).book(book_id).account(account_id).line();
+    var getJournalReports = function(org_id, book_id, account_id){
+      return subledger.organization(org_id).book(book_id).account(account_id).line();
+    };
+    var getCredentials = function(){
+      return $http.get('/credentials').success(function(data, error){
+        if(error){
+          return error;
+        }else {
+          credentials = data;
+        }
+      });
     };
     return {
       getSystemBalance: getSystemBalance,
       createAndPostTransaction: createAndPostTransaction,
       getJournalReports: getJournalReports,
+      getCredentials: getCredentials,
+      setCredentials:setCredentials
     };
   }]);
 
