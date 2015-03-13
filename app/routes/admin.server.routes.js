3997e3e6f5529ac8fdd15997696493adfc5d97a3
Abimbola Idowu <abimbola.idowu@andela.co>
2015-03-13 22:10:00 +0100

    restricted admin routes to users with admin roles only

--- app/routes/admin.server.routes.js
+++ app/routes/admin.server.routes.js
@@ -1,11 +1,11 @@
 'use strict';
 
-  var users = require('../../app/controllers/users.server.controller'),
+  var users = require('../../app/controllers/users/users.authorization.server.controller'),
     adminRoles = require('../../app/controllers/admin/roles.server.controller.js'),
     adminUsers = require('../../app/controllers/admin/users.server.controller.js');
 
 module.exports = function(app) {
-  // app.route('/admin/*').all(users.requiresLogin);
+  app.route('/admin/*').all(users.hasAuthorization('admin'));
   app.route('/admin/role/add').post(adminRoles.addRoles);
   app.route('/admin/roles').get(adminRoles.getRoles);
   app.route('/admin/user/role').post(adminRoles.getRoles);
