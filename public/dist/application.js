366030a1509cdff33064fb092fea7d2190b7e56d
Ekwenugo Mirabel <mirabel.ekwenugo@andela.co>
2015-03-13 13:08:30 +0100

    ran grunt build

--- public/dist/application.js
+++ public/dist/application.js
@@ -152,47 +152,42 @@ angular.module('campaign').controller('addCampaignCtrl', ['$scope','toaster', 'b
 ]);
 'use strict';
 
-angular.module('campaign').controller('allCampaignCtrl', ['$scope','$log', '$location','backendService', function ($scope, $location, $log, backendService) {
+angular.module('campaign').controller('allCampaignCtrl', ['$scope', '$location','backendService', function ($scope, $location, backendService) {
+  $scope.Campaigns = [];
+  $scope.criteria = 'created';
+  $scope.currentPage = 1;
+  $scope.itemsPerPage = 21;
+  $scope.totalItems = 1;
 
- $scope.totalItems = 1;
-
-  backendService.getCampaigns()
-  .success(function (data, status, header, config){
-    $scope.campaigns = data;
-    $scope.totalItems = data.length;
-    $scope.filterCampaigns();
-  })
-  .error(function (error, status, header, config){
-    console.log(error);
+  $scope.init = function() {
+    backendService.getCampaigns()
+    .success(function (data, status, header, config){
+      $scope.campaigns = data;
+      $scope.totalItems = data.length;
+      $scope.filterCampaigns();
+    })
+    .error(function (error, status, header, config){
+      return error;
     });
-
+  };
 
   $scope.filterCampaigns = function () {
-
     var begin = (($scope.currentPage - 1) * $scope.itemsPerPage);
     var end = begin + $scope.itemsPerPage;
     $scope.startItems = begin+1;
-    // $scope.endItems = end < $scope.totalItems ? end : $scope.totalItems;
     if(end < $scope.totalItems){
       $scope.endItems = end;
     }else{
       $scope.endItems = $scope.totalItems;
     }
-
     $scope.Campaigns = $scope.campaigns.slice(begin, end);
   };
 
-
-  $scope.currentPage = 1;
-  $scope.itemsPerPage = 21;
-  $scope.Campaigns = [];
-
   $scope.pageChanged = function() {
-    console.log(10);
     $scope.filterCampaigns();
   };
-  }
-]);
+  $scope.init();
+}]);
 'use strict';
 
 /*global moment */
