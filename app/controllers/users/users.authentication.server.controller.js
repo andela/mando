746c961cda348845fbcf99a847c10c9b456d4bee
andela-amaborukoje <adebayo.maborukoje@andela.co>
2015-03-14 15:41:03 +0100

    added authorization to the banker credentials, refactored the banker authentication page and re-indented some files

--- app/controllers/users/users.authentication.server.controller.js
+++ app/controllers/users/users.authentication.server.controller.js
@@ -5,11 +5,11 @@
  */
 var _ = require('lodash'),
 
-	errorHandler = require('../errors.server.controller'),
-	mongoose = require('mongoose'),
-	passport = require('passport'),
-	adminRoles = require('../admin/roles.server.controller.js'),
-	User = mongoose.model('User'),
+  errorHandler = require('../errors.server.controller'),
+  mongoose = require('mongoose'),
+  passport = require('passport'),
+  adminRoles = require('../admin/roles.server.controller.js'),
+  User = mongoose.model('User'),
   subledger = require('../banker.server.controller');
 
 /**
@@ -44,89 +44,8 @@ exports.oauthCallback = function(strategy) {
  * Helper function to save or update a OAuth user profile
  */
 exports.saveOAuthUserProfile = function(req, providerUserProfile, done) {
-
-
-// 	if(!providerUserProfile.email.match(/andela.co$/)) {
-		
-// 			// res.redirect('/');
-// 		var newUser = req.user;
-//   	return done(new Error('User is not from Andela.co'), newUser);
-// 	}
-
-// 	if (!req.user) {
-// 		// Define a search query fields
-// 		var searchMainProviderIdentifierField = 'providerData.' + providerUserProfile.providerIdentifierField;
-// 		var searchAdditionalProviderIdentifierField = 'additionalProvidersData.' + providerUserProfile.provider + '.' + providerUserProfile.providerIdentifierField;
-
-// 		// Define main provider search query
-// 		var mainProviderSearchQuery = {};
-// 		mainProviderSearchQuery.provider = providerUserProfile.provider;
-// 		mainProviderSearchQuery[searchMainProviderIdentifierField] = providerUserProfile.providerData[providerUserProfile.providerIdentifierField];
-
-// 		// Define additional provider search query
-// 		var additionalProviderSearchQuery = {};
-// 		additionalProviderSearchQuery[searchAdditionalProviderIdentifierField] = providerUserProfile.providerData[providerUserProfile.providerIdentifierField];
-
-// 		// Define a search query to find existing user with current provider profile
-// 		var searchQuery = {
-// 			$or: [mainProviderSearchQuery, additionalProviderSearchQuery]
-// 		};
-
-// 		User.findOne(searchQuery, function(err, user) {
-// 			if (err) {
-// 				return done(err);
-// 			} else {
-// 				if (!user) {
-// 					var possibleUsername = providerUserProfile.username || ((providerUserProfile.email) ? providerUserProfile.email.split('@')[0] : '');
-
-	// 				User.findUniqueUsername(possibleUsername, null, function(availableUsername) {
-	// 					user = {
-	// 						firstName: providerUserProfile.firstName,
-	// 						lastName: providerUserProfile.lastName,
-	// 						username: availableUsername,
-	// 						displayName: providerUserProfile.displayName,
-	// 						email: providerUserProfile.email,
-	// 						provider: providerUserProfile.provider,
-	// 						providerData: providerUserProfile.providerData
-	// 					};
-	// 					//updates the user object or create in not existing,
-	// 					//doing this because of the Admin user that would be manually created
-	// 					User.findOneAndUpdate({email: user.email}, user, {upsert: true}, function(err, user) {
-	// 						adminRoles.addRolesToUser(user._id, user._id, 'member', function(err, user) {
-	// 							return done(err, user);
-	// 						});
-	// 					});
-	// 				});
-	// 			} else {
-	// 				return done(err, user);
-	// 			}
-	// 		}
-	// 	});
-	// } else {
-// 		// User is already logged in, join the provider data to the existing user
-// 		var user = req.user;
-
-// 		// Check if user exists, is not signed in using this provider, and doesn't have that provider data already configured
-// 		if (user.provider !== providerUserProfile.provider && (!user.additionalProvidersData || !user.additionalProvidersData[providerUserProfile.provider])) {
-// 			// Add the provider data to the additional provider data field
-// 			if (!user.additionalProvidersData) user.additionalProvidersData = {};
-// 			user.additionalProvidersData[providerUserProfile.provider] = providerUserProfile.providerData;
-
-// 			// Then tell mongoose that we've updated the additionalProvidersData field
-// 			user.markModified('additionalProvidersData');
-
-// 			// And save the user
-// 			user.save(function(err) {
-// 				return done(err, user, '/#!/settings/accounts');
-// 			});
-// 		} else {
-// 			return done(new Error('User is already connected using this provider'), user);
-// 		}
-// 	}
-// =======
-  if(!providerUserProfile.email.match(/andela.co$/)) {
-    
-      // res.redirect('/');
+  if (!providerUserProfile.email.match(/andela.co$/)) {
+    // res.redirect('/');
     var newUser = req.user;
     return done(new Error('User is not from Andela.co'), newUser);
   }
@@ -158,7 +77,7 @@ exports.saveOAuthUserProfile = function(req, providerUserProfile, done) {
           var possibleUsername = providerUserProfile.username || ((providerUserProfile.email) ? providerUserProfile.email.split('@')[0] : '');
 
           User.findUniqueUsername(possibleUsername, null, function(availableUsername) {
-            user = new User({
+            user = {
               firstName: providerUserProfile.firstName,
               lastName: providerUserProfile.lastName,
               username: availableUsername,
@@ -166,31 +85,32 @@ exports.saveOAuthUserProfile = function(req, providerUserProfile, done) {
               email: providerUserProfile.email,
               provider: providerUserProfile.provider,
               providerData: providerUserProfile.providerData
+            };
+            //updates the user object or create in not existing,
+            //doing this because of the Admin user that would be manually created
+            User.findOneAndUpdate({
+              email: user.email
+            }, user, {
+              upsert: true
+            }, function(err, user) {
+              adminRoles.addRolesToUser(user._id, user._id, 'member', function(err, user) {
+                subledger.createAccount({
+                  'description': user.email,
+                  'reference': 'http://andela.co',
+                  'normal_balance': 'credit'
+                }, function(err, account) {
+                  if (err) {
+                    return done(err, user);
+                  } else {
+                    user.account_id = account.active_account.id;
+                    user.save(function(err, user) {
+                      return done(err, user);
+                    });
+                  }
+                });
+              });
             });
-
-            // create subledger account
-            subledger.createAccount({
-              'description': user.email,
-              'reference': 'http://andela.co',
-              'normal_balance': 'debit'
-            }, function(err, account) {
-              if (err) {
-                // fail the transaction
-                return done(err, user);
-              } else {
-                user.account_id = account.active_account.id;
-                // continue with saving the user
-                // user.save(function(err) {   
-                //  return  done(err, user);
-                // });
-             		User.findOneAndUpdate({email: user.email}, user, {upsert: true}, function(err, user) {
-								adminRoles.addRolesToUser(user._id, user._id, 'member', function(err, user) {
-								return done(err, user);
-							});
-						});
-           }
           });
-         });
         } else {
           return done(err, user);
         }
--- app/routes/banker.server.routes.js
+++ app/routes/banker.server.routes.js
@@ -1,11 +1,12 @@
 'use strict';
+var userAuth = require('../../app/controllers/users/users.authorization.server.controller');
 var banker = require('../controllers/banker.server.controller');
 var users = require('../../app/controllers/users.server.controller');
 
-module.exports = function(app){
-  app.route('/identity').get(banker.getIdentity);
-  app.route('/accounts').get(banker.getAllAccounts);
-  app.route('/accounts/:account_id').get(banker.getUniqueAccount);
- // app.route('/account').get(banker.ArchiveAccount);
-  app.route('/credentials').get(banker.getConstants);
+module.exports = function(app) {
+  app.route('/bank/*').all(users.hasAuthorization('banker'));
+  app.route('/bank/identity').get(banker.getIdentity);
+  app.route('/bank/accounts').get(banker.getAllAccounts);
+  app.route('/bank/accounts/:account_id').get(banker.getUniqueAccount);
+  app.route('/bank/credentials').get(banker.getConstants);
 };
