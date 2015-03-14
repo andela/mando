8bc40e91823576cbf6dd69a4904d0e21c422ca10
andela-amaborukoje <adebayo.maborukoje@andela.co>
2015-03-14 15:38:31 +0100

    added authorization to tthe banker page in angular

--- app/routes/banker.server.routes.js
+++ app/routes/banker.server.routes.js
@@ -6,6 +6,6 @@ module.exports = function(app){
   app.route('/identity').get(banker.getIdentity);
   app.route('/accounts').get(banker.getAllAccounts);
   app.route('/accounts/:account_id').get(banker.getUniqueAccount);
-  app.route('/account').get(banker.ArchiveAccount);
+ // app.route('/account').get(banker.ArchiveAccount);
   app.route('/credentials').get(banker.getConstants);
 };
