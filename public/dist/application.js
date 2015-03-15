197f2bd52bfde88b124a02e415fdfc842e831074
Abimbola Idowu <abimbola2010@gmail.com>
2015-03-15 22:53:32 +0100

    minified code

--- public/dist/application.js
+++ public/dist/application.js
@@ -514,7 +514,10 @@ angular.module('banker').factory('bankerFactory', ['$http', function($http) {
   };
 
   var getJournalReports = function(org_id, book_id, account_id) {
-    return subledger.organization(org_id).book(book_id).account(account_id).line();
+      var org = subledger.organization(org_id);
+      var book = org.book(book_id);
+      var account = book.account(account_id);
+      return account.line();
   };
   var getCredentials = function() {
     return $http.get('/bank/credentials').success(function(data, error) {
@@ -758,60 +761,99 @@ angular.module('campaign').controller('editCampaignCtrl', ['$scope','toaster', '
 ]);
 'use strict';
 
-angular.module('campaign').controller('userCampaignsCtrl', ['$scope', 'backendService','toaster','$location','bankerFactory' ,'Authentication', '$stateParams','lodash','credentials',
-function($scope, backendService, toaster, $location, bankerFactory, Authentication, $stateParams,lodash, credentials) {
+angular.module('campaign').controller('userCampaignsCtrl', ['$scope', 'backendService', 'toaster', '$location', 'bankerFactory', 'Authentication', '$stateParams', 'lodash', 'credentials',
+  function($scope, backendService, toaster, $location, bankerFactory, Authentication, $stateParams, lodash, credentials) {
 
-  $scope.myCampaigns = [];
-  $scope.balance = {};
-  $scope.authentication = Authentication;
+    $scope.myCampaigns = [];
+    $scope.balance = {};
+    $scope.authentication = Authentication;
 
-  if (!$scope.authentication.user) {
-    $location.path('/');
-  }
-
-  //checks if user is an admin
-  $scope.isAdmin = lodash.findWhere(Authentication.user.roles, {'roleType': 'admin'}) ? true : false;
-  $scope.isBanker = lodash.findWhere(Authentication.user.roles, {'roleType': 'banker'}) ? true : false;
-  console.log(credentials);
-  var cred = credentials.data;
-   bankerFactory.setCredentials(cred.key_id, cred.secret_id);
-
-  var userid = $scope.authentication.user._id;
-
-  backendService.getUserCampaigns(userid)
-    .success(function(myCampaigns) {
-      $scope.myCampaigns = myCampaigns;
-    })
-    .error(function(error, status, header, config) {
-      //not cool to redirect the user if any error occured, should be improved by
-      //checking for the exact error act base on the error
+    if (!$scope.authentication.user) {
       $location.path('/');
+    }
 
-    });
+    //checks if user is an admin
+    $scope.isAdmin = lodash.findWhere(Authentication.user.roles, {
+      'roleType': 'admin'
+    }) ? true : false;
+    $scope.isBanker = lodash.findWhere(Authentication.user.roles, {
+      'roleType': 'banker'
+    }) ? true : false;
+    console.log(credentials);
+    var cred = credentials.data;
+    bankerFactory.setCredentials(cred.key_id, cred.secret_id);
 
-    //if role = banker use the banker id here else you the user's id$scope.authentication.user.account_id
-      bankerFactory.getSystemBalance(cred.org_id, cred.book_id, cred.bank_id).balance({description: 'USD'}, function(error, apiRes){
-        if (error){
-           toaster.pop('error', 'An Error Occurred'+ error);
-            return;
-        }else{
-          var amount = parseInt(apiRes.balance.value.amount);
-          $scope.balance.amount = amount;
-          $scope.$digest();
-        }
+    var userid = $scope.authentication.user._id;
+
+    backendService.getUserCampaigns(userid)
+      .success(function(myCampaigns) {
+        $scope.myCampaigns = myCampaigns;
+      })
+      .error(function(error, status, header, config) {
+        //not cool to redirect the user if any error occured, should be improved by
+        //checking for the exact error act base on the error
+        $location.path('/');
+
       });
- // };
-  // function to click the show more button on getMoreCampaigns page
-  $scope.limit = 4;
-  $scope.increment = function() {
-    var campaignLength = $scope.myCampaigns.length;
-    $scope.limit = campaignLength;
-  };
 
-  $scope.decrement = function() {
+    //if role = banker use the banker id here else you the user's id$scope.authentication.user.cred.bank_id
+    bankerFactory.getSystemBalance(cred.org_id, cred.book_id, cred.bank_id).balance({
+      description: 'USD'
+    }, function(error, apiRes) {
+      if (error) {
+        toaster.pop('error', 'An Error Occurred' + error);
+        return;
+      } else {
+        var amount = parseInt(apiRes.balance.value.amount);
+        $scope.balance.amount = amount;
+        $scope.$digest();
+      }
+    });
+    // };
+    // function to click the show more button on getMoreCampaigns page
     $scope.limit = 4;
-  };
-}]);
+    $scope.increment = function() {
+      var campaignLength = $scope.myCampaigns.length;
+      $scope.limit = campaignLength;
+    };
+
+    $scope.decrement = function() {
+      $scope.limit = 4;
+    };
+
+    //Getting all transactions in the system for a particular banker.
+    $scope.getJournals = function(cb) {
+   bankerFactory.getJournalReports(cred.org_id, cred.book_id, cred.bank_id).get({
+     'description': 'USD',
+     'action': 'before',
+     'effective_at': new Date().toISOString()
+   }, function(error, apiRes) {
+     if (error) {
+       return error;
+     } else {
+       for (var i = 0; i < apiRes.posted_lines.length; i++) {
+         try {
+           var stringToObj = JSON.parse(apiRes.posted_lines[i].description);
+           apiRes.posted_lines[i].description = stringToObj;
+         } catch (e) {
+           apiRes.posted_lines[i].description = {
+             'name': 'anonymous',
+             'description': apiRes.posted_lines[i].description
+           };
+         }
+       }
+       $scope.journal = apiRes.posted_lines;
+       $scope.$digest();
+       if (!!cb) {
+         cb();
+       }
+     }
+   });
+ };
+ $scope.getJournals();
+  }
+]);
+
 'use strict';
 
 angular.module('campaign').controller('viewCampaignCtrl', ['$scope','toaster' , 'backendService','$location', 'Authentication', '$stateParams',
