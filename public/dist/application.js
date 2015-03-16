04c13a64ee048fc5f68a46347b3707d47969cee3
andela-amaborukoje <adebayo.maborukoje@andela.co>
2015-03-16 14:41:12 +0100

    ran grunt build

--- public/dist/application.js
+++ public/dist/application.js
@@ -316,7 +316,7 @@ angular.module('banker').config(['$stateProvider', function($stateProvider){
 
 'use strict';
 /*global Subledger*/
-angular.module('banker').controller('transactionCtrl', ['$scope', 'Authentication', '$http', '$timeout', 'toaster', '$modal', 'bankerFactory', 'lodash', 'credentials', '$state', function($scope, Authentication, $http, $timeout, toaster, $modal, bankerFactory, lodash, credentials, $state) {
+angular.module('banker').controller('transactionCtrl', ['$scope', 'Authentication', '$http', '$timeout', 'toaster', '$modal', 'bankerFactory', 'lodash', 'credentials', '$state', function ($scope, Authentication, $http, $timeout, toaster, $modal, bankerFactory, lodash, credentials, $state) {
 
   Authentication.requireLogin($state);
   Authentication.requireRole($state, 'admin', 'userCampaigns');
@@ -493,37 +493,38 @@ angular.module('banker').controller('transactionCtrl', ['$scope', 'Authenticatio
 
 }]);
 
-      'use strict';
+'use strict';
 
-     //modal Controller
-  angular.module('banker').controller('withdrawalModalInstanceCtrl', ['$scope', '$modalInstance', 'transaction', function($scope, $modalInstance, transaction){
-    $scope.systemBalance = transaction;
-    $scope.checkBalance = function () {
-       if($scope.systemBalance < $scope.withdraw) {
-           $scope.accountIsLower = true;
-           $scope.message= true;
-          }else {
-            $scope.accountIsLower =false;
-            $scope.message = false;
-          }      
-    };
+//modal Controller
+angular.module('banker').controller('withdrawalModalInstanceCtrl', ['$scope', '$modalInstance', 'transaction', function($scope, $modalInstance, transaction) {
+  $scope.withdraw = 0;
+  $scope.systemBalance = transaction;
+  $scope.checkBalance = function() {
+    if ($scope.systemBalance < $scope.withdraw) {
+      $scope.accountIsLower = true;
+      $scope.message = true;
+    } else {
+      $scope.accountIsLower = false;
+      $scope.message = false;
+    }
+  };
+  $scope.ok = function(amount) {
+    $modalInstance.close(amount);
+  };
+  $scope.cancel = function() {
+    $modalInstance.dismiss('cancel');
+  };
+}]);
 
-      $scope.ok = function (amount) {
-           $modalInstance.close(amount);
-        };
-        $scope.cancel = function () {
-          $modalInstance.dismiss('cancel');
-        };
-  }]);    
-
-angular.module('banker').controller('depositModalInstanceCtrl', ['$scope', '$modalInstance', function($scope, $modalInstance ){
-      $scope.ok = function (amount) {
-           $modalInstance.close(amount);
-        };
-        $scope.cancel = function () {
-          $modalInstance.dismiss('cancel');
-        };
-  }]);    
+angular.module('banker').controller('depositModalInstanceCtrl', ['$scope', '$modalInstance', function($scope, $modalInstance) {
+  $scope.deposit = 0;
+  $scope.ok = function(amount) {
+    $modalInstance.close(amount);
+  };
+  $scope.cancel = function() {
+    $modalInstance.dismiss('cancel');
+  };
+}]);
 
 'use strict';
 /*global Subledger*/
