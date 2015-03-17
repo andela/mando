886423972700e8ad734578e3fbbbdd323232d7a8
andela-amaborukoje <adebayo.maborukoje@andela.co>
2015-03-18 00:10:49 +0100

    user can see their transaction history on myTransaction page

--- public/modules/campaigns/config/campaign.client.routes.js
+++ public/modules/campaigns/config/campaign.client.routes.js
@@ -6,15 +6,15 @@ angular.module('campaign').config(['$stateProvider', 'datepickerConfig', '$sceDe
   datepickerConfig.startingDay = '1';
 
   $stateProvider.
-    state('addCampaign', {
-      url: '/campaign/add',
-      templateUrl: 'modules/campaigns/views/addCampaign.client.view.html'
-    }).
-    state('editCampaign', {
-      url: '/campaign/:campaignTimestamp/:campaignslug/edit',
-      templateUrl: 'modules/campaigns/views/editCampaign.client.view.html'
-    }).
-    state('viewCampaign', {
+  state('addCampaign', {
+    url: '/campaign/add',
+    templateUrl: 'modules/campaigns/views/addCampaign.client.view.html'
+  }).
+  state('editCampaign', {
+    url: '/campaign/:campaignTimestamp/:campaignslug/edit',
+    templateUrl: 'modules/campaigns/views/editCampaign.client.view.html'
+  }).
+  state('viewCampaign', {
     url: '/campaign/:campaignTimeStamp/:campaignslug',
     resolve: {
       credentials: function($http) {
@@ -24,30 +24,30 @@ angular.module('campaign').config(['$stateProvider', 'datepickerConfig', '$sceDe
     templateUrl: 'modules/campaigns/views/viewCampaign.client.view.html',
     controller: 'viewCampaignCtrl'
   }).
-    state('allCampaigns', {
-      url: '/campaigns',
-      templateUrl: 'modules/campaigns/views/allCampaigns.client.view.html'
-    }).
-    state('allTransactions', {
-      resolve: {
-        credentials: function ($http){
-          return  $http.get('/bank/credentials');
-        }
-      },
-      controller:'userTransactionCtrl',
-      url: '/myTransactions',
-      templateUrl: 'modules/campaigns/views/userTransaction.client.view.html',
-    }).
-    state('userCampaigns', {
-      resolve: {
-        credentials: function ($http){
-          return  $http.get('/bank/credentials');
-        }
-      },
-      url: '/campaigns/myAndonation',
-      templateUrl: 'modules/campaigns/views/userCampaigns.client.view.html',
-      controller: 'userCampaignsCtrl'
-    });
+  state('allCampaigns', {
+    url: '/campaigns',
+    templateUrl: 'modules/campaigns/views/allCampaigns.client.view.html'
+  }).
+  state('allTransactions', {
+    resolve: {
+      credentials: function($http) {
+        return $http.get('/bank/credentials');
+      }
+    },
+    controller: 'userTransactionCtrl',
+    url: '/myTransactions',
+    templateUrl: 'modules/campaigns/views/userTransaction.client.view.html',
+  }).
+  state('userCampaigns', {
+    resolve: {
+      credentials: function($http) {
+        return $http.get('/bank/credentials');
+      }
+    },
+    url: '/campaigns/myAndonation',
+    templateUrl: 'modules/campaigns/views/userCampaigns.client.view.html',
+    controller: 'userCampaignsCtrl'
+  });
   //Add YouTube to resource whitelist so that we can embed YouTube videos
   $sceDelegateProvider.resourceUrlWhitelist(['**']);
 }]);
--- public/modules/campaigns/controllers/userCampaigns.client.controller.js
+++ public/modules/campaigns/controllers/userCampaigns.client.controller.js
@@ -1,6 +1,7 @@
 'use strict';
 
 angular.module('campaign').controller('userCampaignsCtrl', ['$scope', 'backendService', 'toaster', '$location', 'subledgerServices', 'Authentication', '$stateParams', 'lodash', 'credentials', '$state',
+<<<<<<< HEAD
     function($scope, backendService, toaster, $location, subledgerServices, Authentication, $stateParams, lodash, credentials, $state) {
 
         $scope.myCampaigns = [];
@@ -71,4 +72,78 @@ angular.module('campaign').controller('userCampaignsCtrl', ['$scope', 'backendSe
             $scope.limit = 4;
         };
     }
+=======
+  function($scope, backendService, toaster, $location, subledgerServices, Authentication, $stateParams, lodash, credentials, $state) {
+
+    $scope.myCampaigns = [];
+    $scope.systemBalance = {};
+    $scope.balance = {};
+    $scope.authentication = Authentication;
+    Authentication.requireLogin($state);
+    //checks if user is an admin
+    $scope.isAdmin = Authentication.hasRole('admin');
+    $scope.isBanker = Authentication.hasRole('banker');
+    var cred = credentials.data;
+    subledgerServices.setCredentials(cred);
+
+    $scope.isDistributor = Authentication.hasRole('distributor');
+
+    //uses the Currently signed-in id to get the user id.
+    var userid = $scope.authentication.user._id;
+
+    backendService.getUserCampaigns(userid)
+      .success(function(myCampaigns) {
+        $scope.myCampaigns = myCampaigns;
+      })
+      .error(function(error, status, header, config) {
+        //not cool to redirect the user if any error occured, should be improved by
+        //checking for the exact error act base on the error
+        $location.path('/');
+
+      });
+
+    $scope.getCurrentBalance = function(account, destination) {
+      subledgerServices.getBalance(account, function(response) {
+        destination.amount = response;
+        $scope.$digest();
+      });
+    };
+    $scope.getCurrentBalance(cred.bank_id, $scope.systemBalance);
+    $scope.getCurrentBalance($scope.authentication.user.account_id, $scope.balance);
+
+    //GET UNIQUE USER JOURNAL REPORTS  
+    //this methods should only load the bank and  another method will load the user and filter it by 
+    //query seems to be undefined here 
+    $scope.getJournals = function(account) {
+      subledgerServices.getJournals(account, function(response) {
+        $scope.journal = response.posted_lines;
+        url();
+        $scope.$digest();
+      });
+    };
+    $scope.getJournals($scope.authentication.user.account_id);
+
+    //this function set the url of the user balance according to the number of transaction in the url
+    var url = function() {
+      if ($scope.journal.length < 3) {
+        $scope.url = '#here';
+      } else {
+        $scope.url = '/#!/myTransactions';
+        return $scope.url;
+      }
+    };
+
+
+    // function to click the show more button on getMoreCampaigns page
+    $scope.limit = 4;
+    $scope.increment = function() {
+      var campaignLength = $scope.myCampaigns.length;
+      $scope.limit = campaignLength;
+    };
+
+    $scope.decrement = function() {
+      $scope.limit = 4;
+    };
+  }
+>>>>>>> user can see their transaction history on myTransaction page
 ]);
