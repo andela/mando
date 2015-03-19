41017bbd6cda060889bb54f27f488ddcd54102ff
andela-amaborukoje <adebayo.maborukoje@andela.co>
2015-03-19 17:29:00 +0100

    distributor can view users transaction history.

--- public/modules/distributor/controllers/user.distributor.client.controller.js
+++ public/modules/distributor/controllers/user.distributor.client.controller.js
@@ -1,26 +1,41 @@
 'use strict';
 
-angular.module('distributor').controller('userDistributionCtrl', ['$scope', '$http', 'Authentication', 'toaster','subledgerServices','distributorServices','$stateParams', function($scope, $http, Authentication, toaster, subledgerServices, distributorServices , $stateParams) {
+angular.module('distributor').controller('userDistributionCtrl', ['$scope', '$http', 'Authentication', 'toaster','subledgerServices','distributorServices','$stateParams','credentials', '$state', function($scope, $http, Authentication, toaster, subledgerServices, distributorServices , $stateParams, credentials, $state) {
+  $scope.user = {};
+  $scope.journal = [];
 
   var username = $stateParams.username;
   $scope.authentication = Authentication;
-  Authentication.hasRole('distributor');
+  Authentication.requireLogin($state);
+  Authentication.requireRole($state, 'banker', 'userCampaigns');
+  $scope.isDistributor = Authentication.hasRole('distributor');
+  var cred = credentials.data;
+  subledgerServices.setCredentials(cred);
 
-  // $scope.getJournals = function(account) {
-  //   subledgerServices.getJournals(account, function(response) {
-  //     $scope.journal = response.posted_lines;
-  //      $scope.$digest();
-  //   });
-  // };
-  // $scope.getJournals();
+  subledgerServices.setCredentials(cred);
 
   var getByUsername = (function (username) {
     distributorServices.getByUsername(username).success(function (data, status, header, config){
-      console.log(data);
+   //   $scope.user = data;
+   //   console.log(data);
+      console.log(data.account_id);
+      $scope.getJournals(data.account_id);
     })
     .error(function (error,status, header, config){
       console.log(error);
     });
   })(username);
+
+
+  //get All lines of transaction
+  $scope.getJournals = function(account) {
+    subledgerServices.getJournals(account, function(response) {
+      $scope.journal = response.posted_lines;
+      $scope.$digest();
+      console.log($scope.journal);
+    });
+  };
+
+  // $scope.getJournals($scope.user.account_id);
 }]);
 
