466c479e902229a91e47ef8122440874e2777301
Abimbola Idowu <abimbola2010@gmail.com>
2015-03-19 22:26:05 +0100

    ran grunt build

--- public/dist/application.js
+++ public/dist/application.js
@@ -584,45 +584,51 @@ angular.module('campaign').config(['$stateProvider', 'datepickerConfig', '$sceDe
   datepickerConfig.startingDay = '1';
 
   $stateProvider.
-    state('addCampaign', {
-      url: '/campaign/add',
-      templateUrl: 'modules/campaigns/views/addCampaign.client.view.html'
-    }).
-    state('editCampaign', {
-      url: '/campaign/:campaignTimestamp/:campaignslug/edit',
-      templateUrl: 'modules/campaigns/views/editCampaign.client.view.html'
-    }).
-    state('viewCampaign', {
-      url: '/campaign/:campaignTimeStamp/:campaignslug',
-      templateUrl: 'modules/campaigns/views/viewCampaign.client.view.html'
-    }).
-    state('allCampaigns', {
-      url: '/campaigns',
-      templateUrl: 'modules/campaigns/views/allCampaigns.client.view.html'
-    }).
-    state('allTransactions', {
-      resolve: {
-        credentials: ["$http", function ($http){
-          return  $http.get('/bank/credentials');
-        }]
-      },
-      controller:'userTransactionCtrl',
-      url: '/myTransactions',
-      templateUrl: 'modules/campaigns/views/userTransaction.client.view.html',
-    }).
-    state('userCampaigns', {
-      resolve: {
-        credentials: ["$http", function ($http){
-          return  $http.get('/bank/credentials');
-        }]
-      },
-      url: '/campaigns/myAndonation',
-      templateUrl: 'modules/campaigns/views/userCampaigns.client.view.html',
-      controller: 'userCampaignsCtrl'
-    });
+  state('addCampaign', {
+    url: '/campaign/add',
+    templateUrl: 'modules/campaigns/views/addCampaign.client.view.html'
+  }).
+  state('editCampaign', {
+    url: '/campaign/:campaignTimestamp/:campaignslug/edit',
+    templateUrl: 'modules/campaigns/views/editCampaign.client.view.html'
+  }).
+  state('viewCampaign', {
+    url: '/campaign/:campaignTimeStamp/:campaignslug',
+    resolve: {
+      credentials: ["$http", function($http) {
+        return $http.get('/bank/credentials');
+      }]
+    },
+    templateUrl: 'modules/campaigns/views/viewCampaign.client.view.html',
+    controller: 'viewCampaignCtrl'
+  }).
+  state('allCampaigns', {
+    url: '/campaigns',
+    templateUrl: 'modules/campaigns/views/allCampaigns.client.view.html'
+  }).
+  state('allTransactions', {
+    resolve: {
+      credentials: ["$http", function($http) {
+        return $http.get('/bank/credentials');
+      }]
+    },
+    controller: 'userTransactionCtrl',
+    url: '/myTransactions',
+    templateUrl: 'modules/campaigns/views/userTransaction.client.view.html',
+  }).
+  state('userCampaigns', {
+    resolve: {
+      credentials: ["$http", function($http) {
+        return $http.get('/bank/credentials');
+      }]
+    },
+    url: '/campaigns/myAndonation',
+    templateUrl: 'modules/campaigns/views/userCampaigns.client.view.html',
+    controller: 'userCampaignsCtrl'
+  });
 
-    //Add YouTube to resource whitelist so that we can embed YouTube videos
-    $sceDelegateProvider.resourceUrlWhitelist(['**']);
+  //Add YouTube to resource whitelist so that we can embed YouTube videos
+  $sceDelegateProvider.resourceUrlWhitelist(['**']);
 }]);
 
 'use strict';
@@ -808,77 +814,107 @@ angular.module('campaign').controller('editCampaignCtrl', ['$scope', 'toaster', 
 
 'use strict';
 
+angular.module('campaign').controller('supportCampaignCtrl', ['$scope', 'campaignAccountId', 'amountNeeded', 'subledgerServices', 'Authentication', '$modalInstance', function($scope, campaignAccountId, amountNeeded, subledgerServices, Authentication, $modalInstance) {
+
+  subledgerServices.getBalance(Authentication.user.account_id, function(response) {
+    $scope.userAccountBalance = response;
+    $scope.$digest();
+  });
+  subledgerServices.getBalance(campaignAccountId, function(response) {
+    $scope.campaignBalance = response;
+    $scope.$digest();
+  });
+  $scope.amountNeeded = amountNeeded;
+
+  $scope.ok = function() {
+    var transaction = {
+      amount: $scope.amount,
+      reason: 'Support campaign'
+    };
+    subledgerServices.bankerAction('credit', transaction, Authentication.user.account_id, campaignAccountId, Authentication.user, function() {
+      $modalInstance.close(true);
+    });
+  };
+  $scope.cancel = function() {
+    $modalInstance.dismiss('cancelled');
+  };
+}]);
+
+'use strict';
+
 angular.module('campaign').controller('userCampaignsCtrl', ['$scope', 'backendService', 'toaster', '$location', 'subledgerServices', 'Authentication', '$stateParams', 'lodash', 'credentials', '$state',
-    function($scope, backendService, toaster, $location, subledgerServices, Authentication, $stateParams, lodash, credentials, $state) {
+  function($scope, backendService, toaster, $location, subledgerServices, Authentication, $stateParams, lodash, credentials, $state) {
 
-        $scope.myCampaigns = [];
-        $scope.systemBalance = {};
-        $scope.balance = {};
-        $scope.journal = [];
-        $scope.myJournal = [];
-        $scope.authentication = Authentication;
-        Authentication.requireLogin($state);
-        //checks if user is an admin
-        $scope.isAdmin = Authentication.hasRole('admin');
-        $scope.isBanker = Authentication.hasRole('banker');
-        $scope.query = $scope.authentication.user.displayName;
-        var cred = credentials.data;
-        subledgerServices.setCredentials(cred);
+    $scope.myCampaigns = [];
+    $scope.systemBalance = {};
+    $scope.balance = {};
+    $scope.authentication = Authentication;
+    Authentication.requireLogin($state);
+    //checks if user is an admin
+    $scope.isAdmin = Authentication.hasRole('admin');
+    $scope.isBanker = Authentication.hasRole('banker');
+    var cred = credentials.data;
+    subledgerServices.setCredentials(cred);
 
-        $scope.isDistributor = Authentication.hasRole('distributor');
+    $scope.isDistributor = Authentication.hasRole('distributor');
 
-        //uses the Currently signed-in id to get the user id.
-        var userid = $scope.authentication.user._id;
+    //uses the Currently signed-in id to get the user id.
+    var userid = $scope.authentication.user._id;
 
-        backendService.getUserCampaigns(userid)
-            .success(function(myCampaigns) {
-                $scope.myCampaigns = myCampaigns;
-            })
-            .error(function(error, status, header, config) {
-                //not cool to redirect the user if any error occured, should be improved by
-                //checking for the exact error act base on the error
-                $location.path('/');
-            });
+    backendService.getUserCampaigns(userid)
+      .success(function(myCampaigns) {
+        $scope.myCampaigns = myCampaigns;
+      })
+      .error(function(error, status, header, config) {
+        //not cool to redirect the user if any error occured, should be improved by
+        //checking for the exact error act base on the error
+        $location.path('/');
 
-        $scope.getCurrentBalance = function(account, destination) {
-            subledgerServices.getBalance(account, function(response) {
-                destination.amount = response;
-                $scope.$digest();
-            });
-        };
-        $scope.getCurrentBalance(cred.bank_id, $scope.systemBalance);
-        $scope.getCurrentBalance($scope.authentication.user.account_id, $scope.balance);
+      });
 
-        //GET UNIQUE USER JOURNAL REPORTS  
-        //this methods should only load the bank and  another method will load the user and filter it by 
-        //query seems to be undefined here 
-        $scope.getJournals = function(account, cb) {
-            subledgerServices.getJournals(account, function(response) {
-                response = response.posted_lines;
-                cb(response);
-            //    url();
+    $scope.getCurrentBalance = function(account, destination) {
+      subledgerServices.getBalance(account, function(response) {
+        destination.amount = response;
+        $scope.$digest();
+      });
+    };
+    $scope.getCurrentBalance(cred.bank_id, $scope.systemBalance);
+    $scope.getCurrentBalance($scope.authentication.user.account_id, $scope.balance);
 
-            });
-        };
-        $scope.getJournals(cred.bank_id, function(response) {
-            $scope.journal = response;
-            $scope.$digest();
-        });
-        $scope.getJournals($scope.authentication.user.account_id, function(response) {
-            $scope.myJournal = response;
-            $scope.$digest();
-        });
-        // function to click the show more button on getMoreCampaigns page
-        $scope.limit = 4;
-        $scope.increment = function() {
-            var campaignLength = $scope.myCampaigns.length;
-            $scope.limit = campaignLength;
-        };
+    //GET UNIQUE USER JOURNAL REPORTS  
+    //this methods should only load the bank and  another method will load the user and filter it by 
+    //query seems to be undefined here 
+    $scope.getJournals = function(account) {
+      subledgerServices.getJournals(account, function(response) {
+        $scope.journal = response.posted_lines;
+        url();
+        $scope.$digest();
+      });
+    };
+    $scope.getJournals($scope.authentication.user.account_id);
 
-        $scope.decrement = function() {
-            $scope.limit = 4;
-        };
-    }
+    //this function set the url of the user balance according to the number of transaction in the url
+    var url = function() {
+      if ($scope.journal.length < 3) {
+        $scope.url = '#here';
+      } else {
+        $scope.url = '/#!/myTransactions';
+        return $scope.url;
+      }
+    };
+
+
+    // function to click the show more button on getMoreCampaigns page
+    $scope.limit = 4;
+    $scope.increment = function() {
+      var campaignLength = $scope.myCampaigns.length;
+      $scope.limit = campaignLength;
+    };
+
+    $scope.decrement = function() {
+      $scope.limit = 4;
+    };
+  }
 ]);
 
 'use strict';
@@ -914,18 +950,59 @@ angular.module('campaign').controller('userTransactionCtrl', ['$scope', '$http',
 
 'use strict';
 
-angular.module('campaign').controller('viewCampaignCtrl', ['$scope','toaster' , 'backendService','$location', 'Authentication', '$stateParams',
-function($scope, toaster, backendService,$location, Authentication, $stateParams) {
-  $scope.authentication = Authentication;
+angular.module('campaign').controller('viewCampaignCtrl', ['credentials', '$scope', 'toaster', 'backendService', '$location', 'Authentication', '$stateParams', '$modal', 'subledgerServices',
+  function(credentials, $scope, toaster, backendService, $location, Authentication, $stateParams, $modal, subledgerServices) {
+    var campaignBalance, userAccountBalance;
+    $scope.authentication = Authentication;
+    var cred = credentials.data;
+    subledgerServices.setCredentials(cred);
+    backendService.getCampaign($stateParams.campaignTimeStamp + '/' + $stateParams.campaignslug)
+      .success(function(data, status, header, config) {
+        $scope.campaign = data;
+        getCampaignBalance($scope.campaign.account_id);
+        getUserAccountBalance(Authentication.user.account_id);
+      })
+      .error(function(error, status, header, config) {
+        $location.path('/');
+      });
 
-  backendService.getCampaign($stateParams.campaignTimeStamp + '/' + $stateParams.campaignslug)
-  .success(function(data, status, header, config) {
-    $scope.campaign = data;
-  })
-  .error(function(error, status, header, config) {
-    $location.path('/');
-  });
-}]);
+    var getUserAccountBalance = function(userAccountid) {
+      subledgerServices.getBalance(userAccountid, function(response) {
+        userAccountBalance = response;
+      });
+    };
+    var getCampaignBalance = function(campaignAccountid) {
+      subledgerServices.getBalance(campaignAccountid, function(response) {
+        $scope.campaignBalance = response;
+      });
+    };
+    $scope.openModal = function() {
+      $scope.modalInstance = $modal.open({
+        templateUrl: 'modules/campaigns/views/supportCampaign.modal.client.view.html',
+        controller: 'supportCampaignCtrl',
+        size: 'sm',
+        resolve: {
+          campaignAccountId: function() {
+            return $scope.campaign.account_id;
+          },
+          amountNeeded: function() {
+            return $scope.campaign.amount;
+          },
+        }
+      });
+      $scope.modalInstance.result.then(function(status) {
+        backendService.getCampaign($stateParams.campaignTimeStamp + '/' + $stateParams.campaignslug)
+          .success(function(data, status, header, config) {
+            toaster.pop('success', 'Success! - Thanks for supporting this campaign');
+            $scope.campaign = data;
+            getCampaignBalance($scope.campaign.account_id);
+            getUserAccountBalance(Authentication.user.account_id);
+          });
+      });
+    };
+  }
+]);
+
 'use strict';
 
 angular.module('campaign').factory('backendService', ['$http', function($http) {
