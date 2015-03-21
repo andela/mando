3e180c19e357b1b7a1a109a1f7573cb6f98b7ffa
andela-amaborukoje <adebayo.maborukoje@andela.co>
2015-03-21 12:35:34 +0100

    re-indented and removed unused lines

--- public/modules/campaigns/filter/currency.client.filter.js
+++ public/modules/campaigns/filter/currency.client.filter.js
@@ -1,17 +1,16 @@
 'use strict';
 angular.module('campaign').filter('currencyflt', function() {
   return function cur(num) {
-    if(num) {
-    for (var x = 0; x < num.length; x += 3) {
-      if (x > 2) {
-        num = num.slice(0, -x) + ',' + num.slice(-x);
-        x++;
+    if (num) {
+      for (var x = 0; x < num.length; x += 3) {
+        if (x > 2) {
+          num = num.slice(0, -x) + ',' + num.slice(-x);
+          x++;
+        }
       }
+      num = '$' + num;
+      return num;
     }
-    num = '$' + num;
-    return num;
-  }
-  return '$0';
-};
- // cur('123456789012345678901234567890');
+    return '$0';
+  };
 });
