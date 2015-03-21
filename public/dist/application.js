33f5ec0226f69d997b81ee6cade806aec5a8184e
andela-amaborukoje <adebayo.maborukoje@andela.co>
2015-03-21 19:45:59 +0100

    ran grunt build

--- public/dist/application.js
+++ public/dist/application.js
@@ -1043,19 +1043,18 @@ angular.module('campaign').controller('viewCampaignCtrl', ['credentials', '$scop
 'use strict';
 angular.module('campaign').filter('currencyflt', function() {
   return function cur(num) {
-    if(num) {
-    for (var x = 0; x < num.length; x += 3) {
-      if (x > 2) {
-        num = num.slice(0, -x) + ',' + num.slice(-x);
-        x++;
+    if (num) {
+      for (var x = 0; x < num.length; x += 3) {
+        if (x > 2) {
+          num = num.slice(0, -x) + ',' + num.slice(-x);
+          x++;
+        }
       }
+      num = '$' + num;
+      return num;
     }
-    num = '$' + num;
-    return num;
-  }
-  return '$0';
-};
- // cur('123456789012345678901234567890');
+    return '$0';
+  };
 });
 
 'use strict';
