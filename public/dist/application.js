e382274605579d203dfb346d9a4585f8f27fbdff
andela-amaborukoje <adebayo.maborukoje@andela.co>
2015-03-21 21:48:14 +0100

    ran grunt build

--- public/dist/application.js
+++ public/dist/application.js
@@ -854,79 +854,94 @@ angular.module('campaign').controller('supportCampaignCtrl', ['$scope', 'campaig
 'use strict';
 
 angular.module('campaign').controller('userCampaignsCtrl', ['$scope', 'backendService', 'toaster', '$location', 'subledgerServices', 'Authentication', '$stateParams', 'lodash', 'credentials', '$state',
-    function($scope, backendService, toaster, $location, subledgerServices, Authentication, $stateParams, lodash, credentials, $state) {
+  function($scope, backendService, toaster, $location, subledgerServices, Authentication, $stateParams, lodash, credentials, $state) {
 
-        $scope.myCampaigns = [];
-        $scope.systemBalance = {};
-        $scope.balance = {};
-        $scope.journal = [];
-        $scope.myJournal = [];
-        $scope.authentication = Authentication;
-        Authentication.requireLogin($state);
-        //checks if user is an admin
-        $scope.isAdmin = Authentication.hasRole('admin');
-        $scope.isBanker = Authentication.hasRole('banker');
-        $scope.query = $scope.authentication.user.displayName;
-        var cred = credentials.data;
-        subledgerServices.setCredentials(cred);
+    $scope.myCampaigns = [];
+    $scope.systemBalance = {};
+    $scope.balance = {};
+    $scope.journal = [];
+    $scope.myJournal = [];
+    $scope.authentication = Authentication;
+    Authentication.requireLogin($state);
+    //checks if user is an admin
+    $scope.isAdmin = Authentication.hasRole('admin');
+    $scope.isBanker = Authentication.hasRole('banker');
+    $scope.query = $scope.authentication.user.displayName;
+    var cred = credentials.data;
+    subledgerServices.setCredentials(cred);
 
-        $scope.isDistributor = Authentication.hasRole('distributor');
+    $scope.isDistributor = Authentication.hasRole('distributor');
 
-        //uses the Currently signed-in id to get the user id.
-        var userid = $scope.authentication.user._id;
+    //uses the Currently signed-in id to get the user id.
+    var userid = $scope.authentication.user._id;
 
-        backendService.getUserCampaigns(userid)
-            .success(function(myCampaigns) {
-                $scope.myCampaigns = myCampaigns;
-            })
-            .error(function(error, status, header, config) {
-                //not cool to redirect the user if any error occured, should be improved by
-                //checking for the exact error act base on the error
-                $location.path('/');
-            });
+    backendService.getUserCampaigns(userid)
+      .success(function(data) {
+        $scope.myCampaigns = data;
+        for (var i = 0; i < $scope.myCampaigns.length; i++) {
+          var accountNo = data[i].account_id;
 
-        $scope.getCurrentBalance = function(account, destination) {
-            subledgerServices.getBalance(account, function(response) {
-                destination.amount = response;
-                $scope.$digest();
-            });
-        };
-        $scope.getCurrentBalance(cred.bank_id, $scope.systemBalance);
-        $scope.getCurrentBalance($scope.authentication.user.account_id, $scope.balance);
+          $scope.getCampaignBalance(accountNo, $scope.myCampaigns[i]);
+        }
+        // $scope.myCampaigns = 
+      })
+      .error(function(error, status, header, config) {
+        //not cool to redirect the user if any error occured, should be improved by
+        //checking for the exact error act base on the error
+        $location.path('/');
+      });
 
-        //GET UNIQUE USER JOURNAL REPORTS  
-        $scope.getJournals = function(account, cb) {
-            subledgerServices.getJournals(account, function(response) {
-                response = response.posted_lines;
-                cb(response);
-            });
-        };
+    $scope.getCampaignBalance = function(account, destination) {
+      subledgerServices.getBalance(account, function(response) {
+        destination.raised = response;
 
-        /*
-            This is the method that fetches the  transaction journal for the banker using the bank id
-            If the Authenticated User plays Both Role of A banker and a Distributor the Data will have the same values 
-        */
-        $scope.getJournals(cred.bank_id, function(response) {
-            $scope.journal = response;
-            $scope.$digest();
-        });
+        $scope.$digest();
+      });
+    };
 
-        //This is the method that loads the Transaction journal for the Authenticated User.
-        $scope.getJournals($scope.authentication.user.account_id, function(response) {
-            $scope.myJournal = response;
-            $scope.$digest();
-        });
-        // function to click the show more button on getMoreCampaigns page
-        $scope.limit = 4;
-        $scope.increment = function() {
-            var campaignLength = $scope.myCampaigns.length;
-            $scope.limit = campaignLength;
-        };
+    $scope.getCurrentBalance = function(account, destination) {
+      subledgerServices.getBalance(account, function(response) {
+        destination.amount = response;
+        $scope.$digest();
+      });
+    };
 
-        $scope.decrement = function() {
-            $scope.limit = 4;
-        };
-    }
+    $scope.getCurrentBalance(cred.bank_id, $scope.systemBalance);
+    $scope.getCurrentBalance($scope.authentication.user.account_id, $scope.balance);
+
+    //GET UNIQUE USER JOURNAL REPORTS  
+    $scope.getJournals = function(account, cb) {
+      subledgerServices.getJournals(account, function(response) {
+        response = response.posted_lines;
+        cb(response);
+      });
+    };
+
+    /*
+        This is the method that fetches the  transaction journal for the banker using the bank id
+        If the Authenticated User plays Both Role of A banker and a Distributor the Data will have the same values 
+    */
+    $scope.getJournals(cred.bank_id, function(response) {
+      $scope.journal = response;
+      $scope.$digest();
+    });
+
+    //This is the method that loads the Transaction journal for the Authenticated User.
+    $scope.getJournals($scope.authentication.user.account_id, function(response) {
+      $scope.myJournal = response;
+      $scope.$digest();
+    });
+    // function to click the show more button on getMoreCampaigns page
+    $scope.limit = 4;
+    $scope.increment = function() {
+      var campaignLength = $scope.myCampaigns.length;
+      $scope.limit = campaignLength;
+    };
+
+    $scope.decrement = function() {
+      $scope.limit = 4;
+    };
+  }
 ]);
 
 'use strict';
