3b7a89fee4aa99bb0e7969d16e007eb931eb85f2
andela-mfalade <mayowa.falade@andela.co>
2015-05-05 10:11:04 +0100

    Remove console logs from addcampaigns.client.controller.js file

--- public/modules/campaigns/controllers/addCampaign.client.controller.js
+++ public/modules/campaigns/controllers/addCampaign.client.controller.js
@@ -21,13 +21,11 @@ angular.module('campaign').controller('addCampaignCtrl', ['$scope', 'toaster', '
         .success(function(data, status, header, config) {
           toaster.pop('success', $scope.campaign.title, 'Campaign created successfully');
           $location.path('/campaign/' + data.slug);
-          console.log($scope.campaign.youtubeUrl, 'immediately after clicking');
         })
         .error(function(error, status, header, config) {
           //no $scope.error on the view, need to work on the error
           $scope.error = error;
         });
-    console.log($scope.campaign.youtubeUrl, 'some value sha ');
     };
 
     $scope.validateYoutubeUrl = function(url, isValid) {
