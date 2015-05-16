15d0cba1a8aca2ff69dd5071c86b3eea33d80311
andela-mfalade <mayowa.falade@andela.co>
2015-05-16 20:46:42 +0100

    remove unused location provider

--- public/modules/core/config/core.client.routes.js
+++ public/modules/core/config/core.client.routes.js
@@ -1,10 +1,9 @@
 'use strict';
 
 // Setting up route
-angular.module('core').config(['$stateProvider', '$locationProvider', '$urlRouterProvider',
-	function($stateProvider, $locationProvider, $urlRouterProvider) {
+angular.module('core').config(['$stateProvider', '$urlRouterProvider',
+	function($stateProvider, $urlRouterProvider) {
 		// Redirect to home view when route not found
-		 // $locationProvider.html5Mode(true);
 		$urlRouterProvider.otherwise('/');
 
 		// Home state routing
